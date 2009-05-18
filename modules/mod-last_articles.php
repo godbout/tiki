godbout/tiki

commit 2d81a44867d75f1aec982f3c53167ac7c02b9daa

[ENH] add "offset" parameter so article list doesnt have to start at first one.
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@17911 b456876b-0849-0410-b77d-98878d47e9d5

modules/mod-last_articles.php
@@ -1,6 +1,17 @@
 <?php
 // $Id$
 
+// Copyright (c) 2002-2009, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+/**
+ * Show recent articles.
+ *
+ * 2009-04-10	SEWilco
+ * Add 'offset' parameter in case starting at zero is not wanted.
+ **/
+
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   header("location: index.php");
@@ -31,8 +42,10 @@ if (isset($module_params['showImg'])) {
 if (isset($module_params['showDate']) && $module_params['showDate'] == 'y') {
 	$smarty->assign('showDate','y');
 }
+$offset = isset($module_params['offset']) ? $module_params['offset']+0 : '0';
+$smarty->assign('offset',$offset);
 
-$ranking = $tikilib->list_articles(0,$module_rows,'publishDate_desc', '', '', date("U"), '', $mod_type, $mod_topicId, 'y', $mod_topic, $categId, '', '', $l);
+$ranking = $tikilib->list_articles($offset,$module_rows,'publishDate_desc', '', date("U"), '', $mod_type, $mod_topicId, 'y', $mod_topic, $categId, '', '', $l);
 if (isset($module_params['showHeading']) && $module_params['showHeading'] != 'n') {
 	if ($module_params['showHeading'] == 'y')
 		$module_params['showHeading'] = -1;
@@ -47,3 +60,4 @@ $smarty->assign('absurl', isset($module_params["absurl"]) ? $module_params["absu
 $module_rows = count($ranking["data"]);
 $smarty->assign('module_rows', $module_rows);
 
+?>

commit 0f71dc0f9c8988fe55292b318ec3f4bc7e1b96f2

[FIX] mod last articles: remove offset param from smarty to avoid some bad side effects on all pagination_links when ajax is active (broken in r17911)

modules/mod-last_articles.php
@@ -8,8 +8,6 @@
 /**
  * Show recent articles.
  *
- * 2009-04-10	SEWilco
- * Add 'offset' parameter in case starting at zero is not wanted.
  **/
 
 //this script may only be included - so its better to die if called directly.
@@ -35,7 +33,6 @@ $urlParams = array(
 	'lang' => 'lang',
 	'showImg' => NULL,
 	'showDate' => NULL,
-	'offset' => NULL,
 	'showHeading' => NULL,
 	'nonums' => NULL,
 	'absurl' => NULL
@@ -46,7 +43,7 @@ foreach ( $urlParams as $p => $v ) {
 	$$p = isset($module_params[$p]) ? $module_params[$p] : '';
 }
 
-$offset = (int) $offset;
+$offset = (int) $module_params['offset'];
 if ( $absurl == '' ) $absurl = 'n';
 if ( $nonums == '' ) $nonums = 'n';
 if ( $showHeading != 'n') {