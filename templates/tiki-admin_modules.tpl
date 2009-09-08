godbout/tiki

commit 0ae00c5e0642627f29b4d570f4b8bf8e745857c2

[ENH] Do something with common_params. This fixes nonums showing on all new style modules which I had introduced in r21057. [FIX] rows prompted even for modules ignoring it. only fixed for new style modules. rows are still stored in their own DB field, but are nevertheless treated more or less like any common parameter.
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@21298 b456876b-0849-0410-b77d-98878d47e9d5

templates/tiki-admin_modules.tpl
@@ -156,12 +156,14 @@
 					<input type="text" id="assign_cache" name="assign_cache" value="{$assign_cache|escape}" />
 				</td>
 			</tr>
-			<tr>
-				<td class="formcolor"><label for="assign_rows">{tr}Rows{/tr}</label></td>
-				<td class="formcolor">
-					<input type="text" id="assign_rows" name="assign_rows" value="{$assign_rows|escape}" />
-				</td>
-			</tr>
+			{if isset($assign_info.type) and $assign_info.type eq 'include'}
+				<tr>
+						<td class="formcolor"><label for="assign_rows">{tr}Rows{/tr}</label></td>
+						<td class="formcolor">
+								<input type="text" id="assign_rows" name="assign_rows" value="{$assign_rows|escape}" />
+						</td>
+				</tr>
+			{/if}
 			{if isset($assign_info.type) and $assign_info.type eq 'function'}
 				{foreach from=$assign_info.params key=name item=param}
 					<tr>

tiki-admin_modules.php
@@ -48,7 +48,6 @@ $smarty->assign('assign_name', '');
 $smarty->assign('assign_position', '');
 $smarty->assign('assign_order', '');
 $smarty->assign('assign_cache', 0);
-$smarty->assign('assign_rows', 10);
 $smarty->assign('assign_params', '');
 if (isset($_REQUEST["clear_cache"])) {
     check_ticket('admin-modules');
@@ -78,7 +77,6 @@ if (!empty($_REQUEST['edit_assign'])) {
     //$smarty->assign_by_ref('assign_title',$info["title"]);
     $smarty->assign_by_ref('assign_position', $info["position"]);
     $smarty->assign_by_ref('assign_cache', $info["cache_time"]);
-    $smarty->assign_by_ref('assign_rows', $info["rows"]);
     $smarty->assign_by_ref('assign_type', $info["type"]);
     $smarty->assign_by_ref('assign_order', $cosa);
     $smarty->assign_by_ref('info', $info);
@@ -88,8 +86,13 @@ if (!empty($_REQUEST['edit_assign'])) {
 
 	$modinfo = $modlib->get_module_info( $info['name'] );
 	$modlib->dispatchValues( $info['params'], $modinfo['params'] );
-	if ($modinfo["type"] == "include")
+	if ($modinfo["type"] == "include") {
+		$smarty->assign_by_ref('assign_rows', $info["rows"]);
 		$smarty->assign_by_ref('assign_params', $info["params"]); // For old-style modules
+	} else {
+		if (isset($modinfo['params']['rows']))
+			$modinfo['params']['rows']['value'] = $info["rows"];
+	}
 	$smarty->assign('assign_info', $modinfo);
 }
 if (!empty($_REQUEST['unassign'])) {
@@ -174,11 +177,11 @@ if (isset($_REQUEST["preview"])) {
             $module_rows = $_REQUEST["assign_rows"];
             include ($phpfile);
         } elseif (file_exists($phpfuncfile)) {
-            $module_rows = $_REQUEST["assign_rows"];
+            $module_rows = $_REQUEST['assign_params']["rows"];
             include_once ($phpfuncfile);
             $function = 'module_' . $_REQUEST["assign_name"];
             if( function_exists( $function ) ) {
-                $function( array("name" => $_REQUEST["assign_name"], "position" => $_REQUEST["assign_position"], "ord" => $_REQUEST["assign_order"], "cache_time" => $_REQUEST["assign_cache"], "rows" => $_REQUEST["assign_rows"]), $_REQUEST["assign_params"] ); // Warning: First argument should have all tiki_modules table fields. This is just a best effort
+                $function( array("name" => $_REQUEST["assign_name"], "position" => $_REQUEST["assign_position"], "ord" => $_REQUEST["assign_order"], "cache_time" => $_REQUEST["assign_cache"], "rows" => $module_rows), $_REQUEST["assign_params"] ); // Warning: First argument should have all tiki_modules table fields. This is just a best effort.
             }
         }
 
@@ -196,7 +199,8 @@ if (isset($_REQUEST["preview"])) {
     $smarty->assign_by_ref('assign_position', $_REQUEST["assign_position"]);
     $smarty->assign_by_ref('assign_order', $_REQUEST["assign_order"]);
     $smarty->assign_by_ref('assign_cache', $_REQUEST["assign_cache"]);
-    $smarty->assign_by_ref('assign_rows', $_REQUEST["assign_rows"]);
+	if (isset($_REQUEST["assign_rows"]))
+		$smarty->assign_by_ref('assign_rows', $_REQUEST["assign_rows"]);
     $module_groups = $_REQUEST["groups"];
     $grps = '';
     foreach($module_groups as $amodule) {
@@ -213,12 +217,19 @@ if (isset($_REQUEST["assign"])) {
     check_ticket('admin-modules');
     $_REQUEST["assign"] = urldecode($_REQUEST["assign"]);
     $smarty->assign_by_ref('assign_name', $_REQUEST["assign_name"]);
-    //$smarty->assign_by_ref('assign_title',$_REQUEST["assign_title"]);
     $smarty->assign_by_ref('assign_position', $_REQUEST["assign_position"]);
     $smarty->assign_by_ref('assign_params', $_REQUEST["assign_params"]);
+	$modinfo = $modlib->get_module_info( $_REQUEST['assign_name'] );
     $smarty->assign_by_ref('assign_order', $_REQUEST["assign_order"]);
     $smarty->assign_by_ref('assign_cache', $_REQUEST["assign_cache"]);
-    $smarty->assign_by_ref('assign_rows', $_REQUEST["assign_rows"]);
+
+	if (isset($_REQUEST["assign_rows"])) {
+		$module_rows = $_REQUEST["assign_rows"];
+		$smarty->assign_by_ref('assign_rows', $_REQUEST["assign_rows"]);
+	} elseif (isset($_REQUEST["assign_params"]["rows"]))
+		$module_rows = $_REQUEST["assign_params"]["rows"];
+	else
+		$module_rows = 10;
     $smarty->assign_by_ref('assign_type', $_REQUEST["assign_type"]);
     $module_groups = $_REQUEST["groups"];
     $grps = '';
@@ -226,7 +237,7 @@ if (isset($_REQUEST["assign"])) {
         $grps = $grps . " $amodule ";
     }
     $smarty->assign('module_groups', $grps);
-    $modlib->assign_module(isset($_REQUEST['moduleId']) ? $_REQUEST['moduleId'] : 0, $_REQUEST["assign_name"], '', $_REQUEST["assign_position"], $_REQUEST["assign_order"], $_REQUEST["assign_cache"], $_REQUEST["assign_rows"], serialize($module_groups) , $_REQUEST["assign_params"], $_REQUEST["assign_type"]);
+    $modlib->assign_module(isset($_REQUEST['moduleId']) ? $_REQUEST['moduleId'] : 0, $_REQUEST["assign_name"], '', $_REQUEST["assign_position"], $_REQUEST["assign_order"], $_REQUEST["assign_cache"], $module_rows, serialize($module_groups) , $_REQUEST["assign_params"], $_REQUEST["assign_type"]);
     $logslib->add_log('adminmodules', 'assigned module ' . $_REQUEST["assign_name"]);
     header("location: tiki-admin_modules.php");
 }

lib/modules/modlib.php
@@ -414,6 +414,7 @@ class ModLib extends TikiLib {
 			'cachekeygen' => array( $this, 'createDefaultCacheKey' ),
 			'prefs' => array(),
 			'params' => array(),
+			'common_params' => array()
 		);
 
 		$info = array_merge( $defaults, $info );
@@ -431,10 +432,6 @@ class ModLib extends TikiLib {
 			'decorations' => array(
 				'name' => tra('Decorations'),
 				'description' => tra('?'),
-			),
-			'nonums' => array(
-				'name' => tra('No numbers'),
-				'description' => tra('If set to "y", the module will not number list items.'),
 			),
 			'perspective' => array(
 				'name' => tra('Perspective'),
@@ -483,6 +480,22 @@ class ModLib extends TikiLib {
 				'filter' => 'alpha',
 			),
 		) );
+
+		// Parameters common to several modules, but not all
+		$common_params = array(
+			'nonums' => array(
+				'name' => tra('No numbers'),
+				'description' => tra('If set to "y", the module will not number list items.'),
+			),
+			'rows' => array(
+				'name' => tra('Rows'),
+				'description' => tra('Number of rows, or items, to display.') . ' ' . tra('Default: 10.'),
+			)
+		);
+
+		if ($info['type'] == 'function')
+			foreach($info['common_params'] as $common_param)
+				$info['params'][$common_param] = $common_params[$common_param];
 
 		return $info;
 	}

modules/mod-func-breadcrumb.php
@@ -19,6 +19,7 @@ function module_breadcrumb_info() {
 				'filter' => 'int'
 			),
 		),
+		'common_params' => array('nonums', 'rows')
 	);
 }
 

modules/mod-func-directory_last_sites.php
@@ -25,7 +25,7 @@ function module_directory_last_sites_info() {
 				'description' => tra('If set to "y", displays a button labelled "More" that links to the directory.') . " " . tr('Not set by default.')
 			)
 		),
-		'common_params' => array('nonums')
+		'common_params' => array('nonums', 'rows')
 	);
 }
 

modules/mod-func-featured_links.php
@@ -12,7 +12,7 @@ function module_featured_links_info() {
 		'description' => tra('Displays the site\'s first featured links.'),
 		'prefs' => array( 'feature_featuredLinks' ),
 		'params' => array(),
-		'common_params' => array('nonums')
+		'common_params' => array('nonums', 'rows')
 	);
 }
 

modules/mod-func-forums_last_posts.php
@@ -17,7 +17,7 @@ function module_forums_last_posts_info() {
 				'description' => tra('If set to "y", only displays topics.') . " " . tr('Not set by default.')
 			)
 		),
-		'common_params' => array('nonums')
+		'common_params' => array('nonums', 'rows')
 	);
 }
 

modules/mod-func-forums_most_commented_forums.php
@@ -12,7 +12,7 @@ function module_forums_most_commented_forums_info() {
 		'description' => tra('Displays the specified number of the forums with the most comments.'),
 		'prefs' => array( 'feature_forums' ),
 		'params' => array(),
-		'common_params' => array('nonums')
+		'common_params' => array('nonums', 'rows')
 	);
 }
 

commit 4821be37098e1d476df802aafab14197648b06a9

[FIX] oops, regression from r21298

templates/tiki-admin_modules.tpl
@@ -156,7 +156,7 @@
 					<input type="text" id="assign_cache" name="assign_cache" value="{$assign_cache|escape}" />
 				</td>
 			</tr>
-			{if isset($assign_info.type) and $assign_info.type eq 'include'}
+			{if !isset($assign_info.type) or $assign_info.type neq 'function'}
 				<tr>
 						<td class="formcolor"><label for="assign_rows">{tr}Rows{/tr}</label></td>
 						<td class="formcolor">

tiki-admin_modules.php
@@ -48,6 +48,7 @@ $smarty->assign('assign_name', '');
 $smarty->assign('assign_position', '');
 $smarty->assign('assign_order', '');
 $smarty->assign('assign_cache', 0);
+$smarty->assign('assign_rows', 10);
 $smarty->assign('assign_params', '');
 if (isset($_REQUEST["clear_cache"])) {
     check_ticket('admin-modules');
@@ -86,7 +87,7 @@ if (!empty($_REQUEST['edit_assign'])) {
 
 	$modinfo = $modlib->get_module_info( $info['name'] );
 	$modlib->dispatchValues( $info['params'], $modinfo['params'] );
-	if ($modinfo["type"] == "include") {
+	if ($modinfo["type"] != "function") {
 		$smarty->assign_by_ref('assign_rows', $info["rows"]);
 		$smarty->assign_by_ref('assign_params', $info["params"]); // For old-style modules
 	} else {