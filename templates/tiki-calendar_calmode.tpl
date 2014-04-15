godbout/tiki

commit 3377e84d06f3c4215ae14edaa39f9ed8ee632752

[FIX] Changes added to fix parsing issue in action_calender module Ref: http://dev.tiki.org/item4986
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@48759 b456876b-0849-0410-b77d-98878d47e9d5

templates/tiki-calendar_calmode.tpl
@@ -65,7 +65,7 @@
 			{else}
 				href="#"
 			{/if}
-			>{$cell[w][d].items[item].name|truncate:$trunc:".."|escape|default:"..."}</a>
+			>{$cell[w][d].items[item].name|truncate:$trunc:".."|escape|default:"..."|unescape:"htmlall"}</a>
 			{if $cell[w][d].items[item].web}
 			<a href="{$cell[w][d].items[item].web}" target="_other" class="calweb" title="{$cell[w][d].items[item].web}"><img src="img/icons/external_link.gif" width="7" height="7" alt="&gt;"></a>
 			{/if}

commit 9b0eb9cfc12a0954f5b1cab93f31632741ed04ac

[FIX] calendars: No need to unescape htmlall to restore html tags in event titles (used by action calendar), as htmlall ends up with double encoded html entities, messing up accents etc asused in normal calendars (regression from r48759 - thanks marclaporte)

templates/tiki-calendar_calmode.tpl
@@ -59,7 +59,7 @@
 			{/if}>
 
 			{if $myurl eq "tiki-action_calendar.php" or ($cell[w][d].items[item].startTimeStamp >= $cell[w][d].day or $smarty.section.d.index eq '0' or $cell[w][d].firstDay or $infocals[$cell[w][d].items[item].calendarId].nameoneachday eq 'y')}
-		<a style="padding:1px 3px;color:#{$infocals.$calendarId.customfgcolor}{if $cell[w][d].items[item].status eq '2'}text-decoration:line-through;{/if}"
+		<a style="padding:1px 3px;{if $infocals.$calendarId.customfgcolor}color:#{$infocals.$calendarId.customfgcolor}{/if}{if $cell[w][d].items[item].status eq '2'} text-decoration:line-through;{/if}"
 			{if $myurl eq "tiki-action_calendar.php"}
 				{if $cell[w][d].items[item].modifiable eq "y" || $cell[w][d].items[item].visible eq 'y'}href="{$cell[w][d].items[item].url}"{/if}
 			{elseif $prefs.calendar_sticky_popup neq 'y'}
@@ -67,7 +67,7 @@
 			{else}
 				href="#"
 			{/if}
-			>{$cell[w][d].items[item].name|truncate:$trunc:".."|escape|default:"..."|unescape:"htmlall"}</a>
+			>{$cell[w][d].items[item].name|truncate:$trunc:".."|escape|default:"..."|unescape:"html"}</a>
 			{if $cell[w][d].items[item].web}
 			<a {if $prefs.mobile_mode eq "y"}data-role="button" data-mini="true" {/if}href="{$cell[w][d].items[item].web}" target="_other" class="calweb" title="{$cell[w][d].items[item].web}"><img src="img/icons/external_link.gif" width="7" height="7" alt="&gt;"></a>
 			{/if}