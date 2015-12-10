godbout/tiki

commit b274a04a3fdc920d75bfac91fbb26b332177cd2b

[FIX] Improve interface strings including fixing typos and grammatical errors and using more natural expressions.
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@56931 b456876b-0849-0410-b77d-98878d47e9d5

lib/prefs/feature.php
@@ -280,7 +280,7 @@ function prefs_feature_list($partial = false)
 		),
 		'feature_search' => array(
 			'name' => tra('Unified Search Index'),
-			'description' => tra("Also known as 'Advanced Search'. Enables searching for content on the website, using Tiki-managed index."),
+			'description' => tra('Also known as "Advanced Search". Enables searching for content at the site using a Tiki-managed index.'),
 			'help' => 'Search',
 			'type' => 'flag',
 			'default' => 'n',
@@ -590,7 +590,7 @@ function prefs_feature_list($partial = false)
 		),
 		'feature_purifier' => array(
 			'name' => tra('HTML Purifier'),
-			'description' => tra("HTML Purifier is a standards-compliant HTML filter library written in PHP and integrated in Tiki. HTML Purifier will not only remove all malicious code (better known as XSS) with a thoroughly audited, secure yet permissive whitelist, it will also make sure your documents are standards compliant, something only achievable with a comprehensive knowledge of W3C's specifications."),
+			'description' => tra("HTML Purifier is a standards-compliant HTML filter library written in PHP and integrated in Tiki. HTML Purifier will not only remove all malicious code (better known as XSS) with a thoroughly audited, secure yet permissive whitelist, it will also ensure that your documents are standards-compliant. Keep in mind that HTML Purifier is not HTML5 compatible and may rewrite HTML5 syntax and produce unwanted results."),
 			'hint' => tra('If you use HTML in your wiki page and it gets stripped out or rewritten, make sure your HTML is valid, or de-activate this feature.  Keep in mind that HTML Purifier is not HTML5 compatible and may rewrite HTML5 syntax and produce unwanted results.'),
 			'help' => 'Purifier',
 			'type' => 'flag',
@@ -770,7 +770,7 @@ function prefs_feature_list($partial = false)
 			'view' => 'tiki-referer_stats.php',
 		),
 		'feature_redirect_on_error' => array(
-			'name' => tra('Redirect On Error'),
+			'name' => tra('Redirect on error'),
 			'description' => tra('On an error, refresh to the homepage as specified on the Admin General page.'),
 			'help' => 'Redirect+On+Error',
 			'type' => 'flag',
@@ -785,8 +785,8 @@ function prefs_feature_list($partial = false)
 			'default' => 'n',
 		),
 		'feature_mytiki' => array(
-			'name' => tra("Display 'My Account' in the application menu"),
-			'description' => tra("Display 'My Account' in the application menu"),
+			'name' => tra('Display "My Account" in the application menu'),
+			'description' => tra('Display "My Account" in the application menu'),
 			'help' => 'My Account',
 			'type' => 'flag',
 			'default' => 'n',
@@ -903,8 +903,8 @@ function prefs_feature_list($partial = false)
 			'default' => 'n',
 		),
 		'feature_userlevels' => array(
-			'name' => tra('Menu User Levels'),
-			'description' => tra('Allows the menu options that can be seen by a user to be controlled'),
+			'name' => tra('Menu user levels'),
+			'description' => tra('Enables control of the per-group visibility of menu options'),
 			'help' => 'User+Levels',
 			'type' => 'flag',
 			'default' => 'n',
@@ -1094,7 +1094,7 @@ function prefs_feature_list($partial = false)
 		),
 		'feature_draw_in_userfiles' => array(
 			'name' => tra('Draw in User File Gallery'),
-			'description' => tra("User's drawings are stored in their user files gallery"),
+			'description' => tra("Users' drawings are stored in their user files gallery"),
 			'help' => 'Draw',
 			'dependencies' => array(
 				'feature_draw',
@@ -1591,7 +1591,7 @@ function prefs_feature_list($partial = false)
 			'warning' => tra('experimental'),
 		),
 		'feature_file_galleries_author' => array(
-			'name' => tra("Require file author's name for anonymous uploads"),
+			'name' => tra("Require file creator's name for anonymous uploads"),
             'description' => tra(''),
 			'type' => 'flag',
 			'help' => 'File+Gallery+Config',
@@ -2289,7 +2289,7 @@ function prefs_feature_list($partial = false)
 			'default' => 'n',
 		),
 		'feature_wiki_userpage' => array(
-			'name' => tra("User's page"),
+			'name' => tra("User's information page"),
 			'description' => tra('A personal page for the user to manage.'),
 			'type' => 'flag',
 			'default' => 'n',
@@ -2657,7 +2657,7 @@ function prefs_feature_list($partial = false)
 			'default' => 'n',
 		),
 		'feature_alternate_registration_page' => array(
-			'name' => tra('Alternate Registration Page'),
+			'name' => tra('Alternate registration page'),
 			'description' => tra('Redirects requests for tiki-register.php to an alternate registration page.'),
 			'type' => 'text',
 			'default' => 'tiki-register.php',
@@ -2905,8 +2905,8 @@ function prefs_feature_list($partial = false)
 			'default' => 'y',
 		),
 		'feature_userWizardUsersFieldIds' => array(
-			'name' => tra('Tracker Fields Asked in the User Wizard as User Details'),
-			'description' => tra('Users Information Tracker Fields Asked in the User Wizard as User Details (fieldIds separated with colon)'),
+			'name' => tra('Tracker fields presented in the User Wizard as User Details'),
+			'description' => tra('User\'s information tracker fields presented in the User Wizard as User Details (separate fieldIds with colons)'),
 			'help' => 'User+Wizard',
 			'type' => 'text',
 			'size' => '50',

commit 2f0b177211c0f89b6e393e7cf954be5d25017fa7

[FIX]Typo-like issues (unescaped double quotes in strings and such) producing fatal errors fixed (introduced in r56931 among others, I guess). Pending: the commented-out strings in the rest of language.php files (whenever they attempt to translate that file, if this error is not fixed in them, Tiki will show a fatal error for them).

lang/ca/language.php
@@ -2039,7 +2039,7 @@ $lang = array(
 "Temporarily adjust the time limit to use during %0. Depending on the volume of data, some requests may take longer. Increase the time limit locally to resolve the issue. Use reasonable values" => "Temporalment ajustar el límit de temps per navegar durant el %0. Depenent del volum de dades, algunes sol·licituds poden trigar més temps. Augmentar el límit de temps a nivell local per resoldre el problema. Utilitzeu valors raonables",
 "seconds" => "segons",
 "Users accept internal messages by default" => "Els usuaris poden acceptar missatges interns per defecte",
-""if set, users accept internal messages by default" => "si s'ha configurat, l'usuari accepta missatges interns per omissió",
+"if set, users accept internal messages by default" => "si s'ha configurat, l'usuari accepta missatges interns per omissió",
 "Users can opt-out of internal messages" => "Els usuaris poden optar per missatges interns",
 "if set, Users can opt-out of internal messages" => "Si s'estableix, els usuaris poden optar per missatges interns",
 "ID of category whose child categories are bound to a perspective by areas" => "Aneu de la categoria els fills estan obligats a una perspectiva per àrees",
@@ -2085,7 +2085,7 @@ $lang = array(
 "Number of pixels, \"0\" for no maximum" => "Nombre de píxels, 0 per a cap màxim",
 "Default maximum height for custom article images" => "Alçada màxima predeterminada per les imatges d'articles personalitzats",
 "Sets the maximum height of article images" => "estableix l'altura màxima de les imatges de l'article",
-Default maximum width of custom article images in list mode (on View Articles page)" => "Ample màxim predeterminat per a imatges d'articles personalitzats en la manera llista (a Veure Articles)",
+"Default maximum width of custom article images in list mode (on View Articles page)" => "Ample màxim predeterminat per a imatges d'articles personalitzats en la manera llista (a Veure Articles)",
 "Sets the default maximum width of custom article images in list mode (on View Articles page)" => "estableix l'ample màxim predeterminat per a imatges d'articles personalitzats en la manera de llista (a Veure Articles)",
 "Number of pixels (\"0\" to default to the view mode maximum)" => "Nombre de píxels, 0 per la manera de reserva per màxima vista",
 "Default maximum height of custom article images in list mode (on View Articles page)" => "Alçada màxima predeterminada per les imatges d'articles personalitzats en la manera llista (a Veure Articles)",
@@ -2763,7 +2763,7 @@ Default maximum width of custom article images in list mode (on View Articles pa
 "Latest Changes" => "Darrers canvis",
 "Show the list of last-changed items" => "mostrarà una llista dels últims articles modificats",
 "Orphan pages" => "Pàgines òrfenes",
-"Allow listing of "orphan pages" (pages not linked to from another page)" => "permetre llista d'orfes (no classificades) articles",
+"Allow listing of \"orphan pages\" (pages not linked to from another page)" => "permetre llista d'orfes (no classificades) articles",
 "MySQL full-text search" => "MySQL cerca de text complet",
 "Referer search highlighting" => "Ressaltat dels referenciadors de les cerques",
 "Search statistics" => "Estadístiques de les cerques",

lang/de/language.php
@@ -2085,7 +2085,7 @@ $lang = array(
 "Number of pixels, \"0\" for no maximum" => "Anzahl von Pixel. 0 für keine Begrenzung",
 "Default maximum height for custom article images" => "Standard für die maximale Höhe für benutzerdefinierte Artikelbilder",
 // "Sets the maximum height of article images" => "Sets the maximum height of article images",
-Default maximum width of custom article images in list mode (on View Articles page)" => "Standard für die maximale Breite für benutzerdefinierte Artikelbilder im Listenmodus (bei Artikel ansehen)",
+"Default maximum width of custom article images in list mode (on View Articles page)" => "Standard für die maximale Breite für benutzerdefinierte Artikelbilder im Listenmodus (bei Artikel ansehen)",
 // "Sets the default maximum width of custom article images in list mode (on View Articles page)" => "Sets the default maximum width of custom article images in list mode (on View Articles page)",
 "Number of pixels (\"0\" to default to the view mode maximum)" => "Anzahl von Pixel. 0 um zum Maximum des Ansichtsmodus zurückzukehren",
 "Default maximum height of custom article images in list mode (on View Articles page)" => "Standard für die maximale Höhe für benutzerdefinierte Artikelbilder im Listenmodus (bei Artikel ansehen)",
@@ -2763,7 +2763,7 @@ Default maximum width of custom article images in list mode (on View Articles pa
 "Latest Changes" => "Neueste Änderungen",
 // "Show the list of last-changed items" => "Show the list of last-changed items",
 "Orphan pages" => "Verwaiste Seiten",
-"Allow listing of "orphan pages" (pages not linked to from another page)" => "gestatte das auflisten verwaister (nicht kategorisierter) Artikel",
+"Allow listing of \"orphan pages\" (pages not linked to from another page)" => "gestatte das auflisten verwaister (nicht kategorisierter) Artikel",
 "MySQL full-text search" => "MySQL Volltextsuche",
 "Referer search highlighting" => "Hervorhebung der Suchbegriffe auf den gefundenen Seiten",
 "Search statistics" => "Suchstatistik",

lang/ko/language.php
@@ -2039,7 +2039,7 @@ $lang = array(
 "Temporarily adjust the time limit to use during %0. Depending on the volume of data, some requests may take longer. Increase the time limit locally to resolve the issue. Use reasonable values" => "%0 동안 사용할 시간 제한을 임시적으로 조절. 데이터의 크기에 따라, 어떤 요청은 더 많은 시간을 요구합니다. 시간제한을 지역적으로 증가하여 문제를 해결하십시오. 적절한 값을 사용하십시오",
 "seconds" => "초",
 "Users accept internal messages by default" => "사용자는 기본으로 내부 메시지를 받게됩니다",
-""if set, users accept internal messages by default" => "설정된 경우, 사용자는 기본으로 내부 메시지를 받습니다",
+"if set, users accept internal messages by default" => "설정된 경우, 사용자는 기본으로 내부 메시지를 받습니다",
 "Users can opt-out of internal messages" => "사용자는 내부 메시지에서 옵트아웃(opt-out)할 수 있습니다",
 "if set, Users can opt-out of internal messages" => "설정된 경우, 사용자는 내부 메시지에서 옵트아웃 할 수 있습니다",
 "ID of category whose child categories are bound to a perspective by areas" => "자식들이 영역 별로 Perspective에 묶이게 되는 범주의 ID",
@@ -2085,7 +2085,7 @@ $lang = array(
 "Number of pixels, \"0\" for no maximum" => "픽셀수, 최대 없음은 0",
 "Default maximum height for custom article images" => "사용자 지정 기사 그림에 대한 기본 최대 높이",
 "Sets the maximum height of article images" => "기사 그림에 대하여 최대 높이를 설정함",
-Default maximum width of custom article images in list mode (on View Articles page)" => "목록 모드 내부의 사용자 지정 기사 그림에 대한 기본 최대 넓이 (기사 보기에서)",
+"Default maximum width of custom article images in list mode (on View Articles page)" => "목록 모드 내부의 사용자 지정 기사 그림에 대한 기본 최대 넓이 (기사 보기에서)",
 "Sets the default maximum width of custom article images in list mode (on View Articles page)" => "목록 모드에서 사용자 지정 기사 그림 기본 최대 넓이를 설정 (기사 보기에서)",
 "Number of pixels (\"0\" to default to the view mode maximum)" => "픽셀수, 보기 모드 최대로 대체하려면 0",
 "Default maximum height of custom article images in list mode (on View Articles page)" => "목록 모드 내부의 사용자 지정 기사 그림에 대한 기본 최대 높이 (기사 보기에서)",
@@ -2764,7 +2764,7 @@ Default maximum width of custom article images in list mode (on View Articles pa
 "Latest Changes" => "마지막변경",
 "Show the list of last-changed items" => "최종 변경된 항목들의 목록 표시",
 "Orphan pages" => "고아 페이지",
-"Allow listing of "orphan pages" (pages not linked to from another page)" => "고아된 (범주화되지 않은) 항목들의 나열 허용",
+"Allow listing of \"orphan pages\" (pages not linked to from another page)" => "고아된 (범주화되지 않은) 항목들의 나열 허용",
 "MySQL full-text search" => "MySQL 전체 텍스트 검색",
 "Referer search highlighting" => "참조자 검색 하이라이트 중",
 "Search statistics" => "검색통계",

lib/prefs/allowmsg.php
@@ -10,7 +10,7 @@ function prefs_allowmsg_list()
 	return array(
 		'allowmsg_by_default' => array(
 			'name' => tra('Users accept internal messages by default'),
-			'description' => tra('"if set, users accept internal messages by default'),
+			'description' => tra('if set, users accept internal messages by default'),
 			'type' => 'flag',
 			'dependencies' => array(
 				'feature_messages',

lib/prefs/feature.php
@@ -1424,7 +1424,7 @@ function prefs_feature_list($partial = false)
 		),
 		'feature_listorphanPages' => array(
 			'name' => tra('Orphan pages'),
-            'description' => tra('Allow listing of "orphan pages" (pages not linked to from another page)'),
+            'description' => tra('Allow listing of \"orphan pages\" (pages not linked to from another page)'),
 			'type' => 'flag',
 			'view' => 'tiki-orphan_pages.php',
 			'default' => 'n',