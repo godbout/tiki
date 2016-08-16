godbout/tiki

commit b00ae045ccc030d50b47bf63704f8984ac80a290

[FIX] replace remaining instances of tracker status images with iconsets
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@59391 b456876b-0849-0410-b77d-98878d47e9d5

lib/smarty_tiki/function.object_link.php
@@ -204,7 +204,9 @@ function smarty_function_object_link_trackeritem( $smarty, $object, $title = nul
 
 	if (($show_status == 'y') && $item && $status = $item->getDisplayedStatus()) {
 		$alt = tr($status);
-		$pre = "<img src=\"img/icons/status_$status.gif\" alt=\"$status\"/>&nbsp;";
+//		$pre = "<img src=\"img/icons/status_$status.gif\" alt=\"$status\"/>&nbsp;";
+		$pre = smarty_function_icon(array('name' => 'status-' . $status, 'iclass' => 'tips', 'ititle' => ':'
+				. ucfirst($status)), $smarty);
 	}
 
 	return $pre . smarty_function_object_link_default($smarty, $object, $title, $type, $url);

commit 52f7f9a670ff43628cdc9a28f580b859028ee434

[FIX] trackers: Fix fatal error on object link status for tracker items, regression from r59391 - testing is good, will there be many more of these? :(

lib/smarty_tiki/function.object_link.php
@@ -203,10 +203,12 @@ function smarty_function_object_link_trackeritem( $smarty, $object, $title = nul
 	}
 
 	if (($show_status == 'y') && $item && $status = $item->getDisplayedStatus()) {
-		$alt = tr($status);
-//		$pre = "<img src=\"img/icons/status_$status.gif\" alt=\"$status\"/>&nbsp;";
-		$pre = smarty_function_icon(array('name' => 'status-' . $status, 'iclass' => 'tips', 'ititle' => ':'
-				. ucfirst($status)), $smarty);
+		$smarty->loadPlugin('smarty_function_icon');
+		$pre = smarty_function_icon(array(
+			'name' => 'status-' . $status,
+			'iclass' => 'tips',
+			'ititle' =>  ':' . tr($status),
+		), $smarty);
 	}
 
 	return $pre . smarty_function_object_link_default($smarty, $object, $title, $type, $url);