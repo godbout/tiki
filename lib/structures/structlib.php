godbout/tiki

commit 57b610501480f8204eb3e50bb5106a9525147167

[NEW] Mail-in routing of incoming email to structures. Configured per user in "My Tiki" / "Mail-in" ...the option for this is still pending. [ENH] Mail-in admin panel lists actions before the "Add New" account section. [DOC] Make header like comments for each mail-in type
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@46430 b456876b-0849-0410-b77d-98878d47e9d5

lib/structures/structlib.php
@@ -251,13 +251,22 @@ class StructLib extends TikiLib
       \param alias An alias for the wiki page name.
       \return the new entries page_ref_id or null if not created.
 	*/
-	public function s_create_page($parent_id, $after_ref_id, $name, $alias='', $structure_id=null)
+	public function s_create_page($parent_id, $after_ref_id, $name, $alias='', $structure_id=null, $options = array())
 	{
 		global $prefs;
 		$ret = null;
+		
+		$hide_toc = isset($options['hide_toc']) ? $options['hide_toc'] : 'n';
+		$creator = isset($options['creator']) ? $options['creator'] : tra('system');
+		$creator_msg = isset($options['creator_msg']) ? $options['creator_msg'] : tra('created from structure');
+		$ip_source = isset($options['ip_source']) ? $options['ip_source'] : '0.0.0.0';
+		
 		// If the page doesn't exist then create a new wiki page!
-		$newpagebody = tra("Table of contents") . ":" . "{toc}";
-		$created = $this->create_page($name, 0, $newpagebody, $this->now, tra('created from structure'), 'system', '0.0.0.0', '', false, '', array('parent_id'=>$parent_id));
+		$newpagebody = '';
+		if ($hide_toc !== 'y') {
+			$newpagebody = tra("Table of contents") . ":" . "{toc}";
+		}
+		$created = $this->create_page($name, 0, $newpagebody, $this->now, $creator_msg, $creator, $ip_source, '', false, '', array('parent_id'=>$parent_id));
 
 		if (!empty($parent_id) || $created || ! $this->page_is_in_structure($name)) {
 			// if were not trying to add a duplicate structure head
@@ -721,6 +730,14 @@ class StructLib extends TikiLib
 		$cant = $this->getOne($query, array($pageName));
 		return $cant;
 	}
+	public function page_id_is_in_structure($pageId)
+	{
+		$query  = 'select count(*) ';
+		$query .= 'from `tiki_structures` ts, `tiki_pages` tp ';
+		$query .= 'where ts.`page_id`=tp.`page_id` and `page_id`=?';
+		$cant = $this->getOne($query, array($pageId));
+		return $cant;
+	}
 	//Is this page the head page for a structure?
 	public function get_struct_ref_if_head($pageName)
 	{
@@ -980,7 +997,7 @@ class StructLib extends TikiLib
 		if (!empty($join_bindvars)) {
 			$bindvars = empty($bindvars)? $join_bindvars : array_merge($join_bindvars, $bindvars);
 		}
-		$query = "select `page_ref_id`,`parent_id`,ts.`page_id`,`page_alias`,`pos`,
+		$query = "select `page_ref_id`,`structure_id`,`parent_id`,ts.`page_id`,`page_alias`,`pos`,
 			`pageName`,tp.`hits`,`data`,tp.`description`,`lastModif`,`comment`,`version`,
 			`user`,`ip`,`flag`,`points`,`votes`,`cache`,`wiki_cache`,`cache_timestamp`,
 			`pageRank`,`creator`,`page_size` from `tiki_structures` as ts $join_tables $mid order by ".$this->convertSortMode($sort_mode);
@@ -1196,6 +1213,24 @@ class StructLib extends TikiLib
 			$query = "update `tiki_structures` set `structure_id`=?, `parent_id`=?, `pos`=? where `page_ref_id`=?";
 			$this->query($query, array($structure_id, $structure_id, $pos+1, $page_ref_id));
 		}
+	}
+	public function move_to_structure_child($parent_ref_id, $structure_id, $first=true)
+	{
+		$query = "update `tiki_structures` set `pos`=`pos`-1 where `pos`>? and `parent_id`=?";
+		$this->query($query, array((int) $page_info["pos"], (int) $parent_ref_id));
+		if ($first) {
+			$query = "update `tiki_structures` set `pos`=`pos`+1 where `parent_id`=?";
+			$this->query($query, array($structure_id));
+			$pos = 1;
+			$query = "update `tiki_structures` set `structure_id`=?, `parent_id`=?, `pos`=? where `page_ref_id`=?";
+			$this->query($query, array($structure_id, $structure_id, $pos+1, $page_ref_id));
+		} else {
+			$query = "select max(`pos`) from `tiki_structures` where `parent_id`=?";
+			$pos = $this->getOne($query, array($structure_id));
+			$query = "update `tiki_structures` set `structure_id`=?, `parent_id`=?, `pos`=? where `page_ref_id`=?";
+			$this->query($query, array($structure_id, $structure_id, $pos+1, $page_ref_id));
+		}
+		return true;
 	}
 	/* transform a structure into a menu */
 	public function to_menu($channels, $structure, $sectionLevel=0, $cumul=0, $params=array())

db/tiki.sql
@@ -3755,3 +3755,15 @@ CREATE TABLE `tiki_workspace_templates` (
 	`definition` TEXT,
 	`is_advanced` CHAR(1) NOT NULL DEFAULT 'n'
 ) ENGINE=MyISAM;
+
+DROP TABLE IF EXISTS `tiki_user_mailin_struct`;
+CREATE TABLE `tiki_user_mailin_struct` (
+	`mailin_struct_id` int(12) NOT NULL auto_increment,
+	`username` varchar(200) NOT NULL,
+	`subj_pattern` varchar(255) NULL,
+	`body_pattern` varchar(255) NULL,
+	`structure_id` int(14) NOT NULL,
+	`page_id` int(14) NULL,
+	`is_active` char(1) NULL DEFAULT 'n',
+   PRIMARY KEY (`mailin_struct_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1;

installer/schema/20130623_create_mailin_user_struct_tiki.sql
@@ -1,0 +1,10 @@
+CREATE TABLE `tiki_user_mailin_struct` (
+	`mailin_struct_id` int(12) NOT NULL auto_increment,
+	`username` varchar(200) NOT NULL,
+	`subj_pattern` varchar(255) NULL,
+	`body_pattern` varchar(255) NULL,
+	`structure_id` int(14) NOT NULL,
+	`page_id` int(14) NULL,
+	`is_active` char(1) NULL DEFAULT 'n',
+   PRIMARY KEY (`mailin_struct_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1;

templates/tiki-admin_mailin.tpl
@@ -46,6 +46,34 @@
 {section name=n loop=$tikifeedback}<div class="simplebox {if $tikifeedback[n].num > 0} highlight{/if}">{$tikifeedback[n].mes}</div>{/section}
 {/if}
 <br>
+<a name="check" ></a><h2><a href="tiki-mailin.php#check">{tr}Check Mail-in accounts{/tr}</a></h2>
+<form action="tiki-admin_mailin.php" method="post">
+  <table class="formcolor">
+    <tr>
+      <td>{tr}Check automatically{/tr}</td>
+      <td>
+		&nbsp;&nbsp;
+        {tr}Yes{/tr}
+        <input type="radio" name="mailin_autocheck" {if $prefs.mailin_autocheck eq 'y'}checked="checked"{/if} value="y">
+		&nbsp;&nbsp;
+        {tr}No{/tr}
+        <input type="radio" name="mailin_autocheck" {if $prefs.mailin_autocheck eq 'n'}checked="checked"{/if} value="n">
+      </td>
+    </tr>
+    <tr>
+      <td>{tr}Frequency{/tr}</td>
+      <td><input type="text" name="mailin_autocheckFreq" size="6" value="{$prefs.mailin_autocheckFreq}">&nbsp;{tr}mn{/tr}</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="set_auto" value="{tr}Set{/tr}"></td>
+    </tr>
+    <tr>
+      <td>{tr}Check manually{/tr}</td>
+      <td><a href="tiki-mailin.php">Check Now</a></td>
+    </tr>
+  </table>
+</form>
 
 <a name="add" ></a><h2>{if $accountId eq 0}{tr}Add new Mail-in account{/tr}{else}{tr}Edit Mail-in account:{/tr} <i>{$info.account}</i>{/if}</h2>
 <form action="tiki-admin_mailin.php" method="post">
@@ -236,35 +264,6 @@
     <tr>
       <td>&nbsp;</td>
       <td colspan="4"><input type="submit" name="new_acc" value="{if $accountId eq 0}{tr}Add{/tr}{else}{tr}Save{/tr}{/if}"></td>
-    </tr>
-  </table>
-</form>
-
-<a name="check" ></a><h2><a href="tiki-mailin.php#check">{tr}Check Mail-in accounts{/tr}</a></h2>
-<form action="tiki-admin_mailin.php" method="post">
-  <table class="formcolor">
-    <tr>
-      <td>{tr}Check automatically{/tr}</td>
-      <td>
-		&nbsp;&nbsp;
-        {tr}Yes{/tr}
-        <input type="radio" name="mailin_autocheck" {if $prefs.mailin_autocheck eq 'y'}checked="checked"{/if} value="y">
-		&nbsp;&nbsp;
-        {tr}No{/tr}
-        <input type="radio" name="mailin_autocheck" {if $prefs.mailin_autocheck eq 'n'}checked="checked"{/if} value="n">
-      </td>
-    </tr>
-    <tr>
-      <td>{tr}Frequency{/tr}</td>
-      <td><input type="text" name="mailin_autocheckFreq" size="6" value="{$prefs.mailin_autocheckFreq}">&nbsp;{tr}mn{/tr}</td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-      <td><input type="submit" name="set_auto" value="{tr}Set{/tr}"></td>
-    </tr>
-    <tr>
-      <td>{tr}Check manually{/tr}</td>
-      <td><a href="tiki-mailin.php">Check Now</a></td>
     </tr>
   </table>
 </form>

templates/tiki-mytiki_bar.tpl
@@ -62,5 +62,10 @@
 	{button _icon="img/icons/large/socialnetworks.png" _text="{tr}Social networks{/tr}" href="tiki-socialnetworks.php" _menu_text="y"}
 {/if}
 
+{if $prefs.feature_mailin eq 'y' and !empty($user) and ($tiki_p_send_mailin eq 'y' or $tiki_p_admin_mailin eq 'y')}
+	{button _icon="img/icons/large/green_question48x48.png" _text="{tr}Mail-in{/tr}" href="tiki-user_mailin.php" _menu_text="y"}
+{/if}
+
+
 </div>
 <br/>

tiki-mailin-code.php
@@ -15,6 +15,7 @@ require_once ("lib/webmail/net_pop3.php");
 include_once ("lib/mail/mimelib.php");
 include_once ("lib/webmail/tikimaillib.php");
 include_once ('lib/wiki/wikilib.php');
+require_once ('lib/mailin/usermailinlib.php');
 
 global $prefs;
 
@@ -421,6 +422,9 @@ foreach ($accs['data'] as $acc) {
 							}
 							
 							if ($prefs['feature_articles'] && $acc['type'] == 'article-put') {
+								//////////////
+								//	article-put
+								//////////////////////////////////////////////////////////////////////////////////
 
 								$title = trim($output['header']['subject']);
 								$topicId = isset($acc['article_topicId']) ? $acc['article_topicId'] : 0;
@@ -508,6 +512,9 @@ foreach ($accs['data'] as $acc) {
 								}
 								
 								if ($acc['type'] == 'wiki-get' || ($acc['type'] == 'wiki' && $method == "GET")) {
+									//////////////
+									//	wiki-get, wiki GET: Get a new wiki page. System emails page to user
+									//////////////////////////////////////////////////////////////////////////////////
 									// A wiki-get account sends a copy of the page to the sender
 									// and also sends the source of the page
 									$mail = new TikiMail();
@@ -537,12 +544,31 @@ foreach ($accs['data'] as $acc) {
 									$res = $mail->send(array($email_from), 'mail');
 									$content.= "Response sent<br />";
 									//end if ($acc['type'] == 'wiki-get' || ($acc['type'] == 'wiki' && $method == "GET"))
+
 								} elseif ($acc['type'] == 'wiki-put' || ($acc['type'] == 'wiki' && $method == "PUT")) {
+									//////////////
+									//	wiki-put, wiki PUT: Send a wiki page. User emails page to System
+									//////////////////////////////////////////////////////////////////////////////////
 									
-									// This is used to UPDATE wiki pages
+									// This is used to Create/Update wiki pages
+									$chkUser = $aux["sender"]["user"];
+
+									// Attempt to use HTML, if it exists
+									$body = mailin_get_html($output);
+									if (empty($body)) {
+										$body = mailin_get_body($output);
+									}
+									
+									// Load user routing
+									$route = null;
+									$routes = $usermailinlib->locate_struct($chkUser, $aux['Subject'], $body);
+									if (!empty($routes['data'])) {
+										$content.= "User route from pattern: '".$routes['data'][0]['subj_pattern']."' / '".$routes['data'][0]['body_pattern']."'<br />";
+										$route = $routes['data'][0];	// Only use the first route
+									}
+
 
 									// Check permissions
-									$chkUser = $aux["sender"]["user"];
 									if (($acc["anonymous"] == 'n') && (!$userlib->user_has_permission($chkUser, 'tiki_p_admin'))) {
 										if ($tikilib->page_exists($page)) {
 											// Check permissions for page
@@ -567,6 +593,12 @@ foreach ($accs['data'] as $acc) {
 													$can_addAttachment = 'n';
 													$show_inlineImages = 'n';
 												}
+												if (!empty($routes)) {
+													if (!$userlib->object_has_permission($chkUser, $acc['categoryId'], 'category', 'tiki_p_edit_structures')) {
+														$content.= $chkUser." cannot edit structures: ".$page."<br />";
+														$processEmail = false;
+													}
+												}
 											} else {
 												if (!$userlib->user_has_permission($chkUser, 'tiki_p_edit')) {
 													$content.= $chkUser." cannot create the page: ".$page."<br />";
@@ -576,25 +608,36 @@ foreach ($accs['data'] as $acc) {
 													$can_addAttachment = 'n';
 													$show_inlineImages = 'n';
 												}
+												if (!empty($routes)) {
+													if (!$userlib->user_has_permission($chkUser, 'tiki_p_edit_structures')) {
+														$content.= $chkUser." cannot edit structures: ".$page."<br />";
+														$processEmail = false;
+													}
+												}
 											}
 										}
 									}
 									if ($processEmail) {
 
-										// Attempt to use HTML, if it exists
-										$body = mailin_get_html($output);
-										if (empty($body)) {
-											$body = mailin_get_body($output);
-										}
-										
-										// Add namespace, if specified
+										// Add namespace, if specified.
+										// If no explicit namespace is specified, pages routed to structured may inherit, the structure namespace
 										if ($prefs['namespace_enabled'] === 'y') {
 											$nsName = trim($acc['namespace']);
+											$ns = $prefs['namespace_separator'];
 											if (!empty($nsName)) {
-												$ns = $prefs['namespace_separator'];
+												// Use mail-in specified namespace
 												if (!empty($ns)) {
 													$page = $nsName.$ns.$page;
 												}
+											} elseif (!empty($route)) {
+												// Inherit structure namespace
+												$wikilib = TikiLib::lib('wiki');
+												$nsName = $wikilib->get_namespace($route['pageName']);
+												if (!empty($nsName)) {
+													if (!empty($ns)) {
+														$page = $nsName.$ns.$page;
+													}
+												}												
 											}
 										}
 
@@ -612,38 +655,74 @@ foreach ($accs['data'] as $acc) {
 
 											if (!$tikilib->page_exists($page)) {
 
-												$tikilib->create_page($page, 0, $body, $tikilib->now, "Created from " . $acc["account"], $aux["sender"]["user"],
-																	'0.0.0.0',
-																	'',						//description
-																	'',						//lang
-																	$parsed_data['is_html'],	//is_html
-																	'',						//hash
-																	$parsed_data['wysiwyg']	//wysiwyg
-												);
-												$content.= "Page: $page has been created<br />";
+												// Check User structure routing
+												if (!empty($route)) {
+													// Structure routing is active. Create a structure node/page
+													$parent_id = $route['page_ref_id'];
+													$structure_id = $route['structure_id'];
+													$begin = true;
+														
+													$after_ref_id = null;
+													$alias='';
+													$options = array();
+														
+													$options['hide_toc'] = 'y';
+													$options['creator'] = tra('mail-in');
+													$options['creator_msg'] = tra('created from mail-in');
+													$options['ip_source'] = '0.0.0.0';
+														
+													$structlib = TikiLib::lib('struct');
+													$structlib->s_create_page($parent_id, $after_ref_id, $page, $alias, $structure_id, $options);
+													$content.= "Page: $page has been added to structureId: ".$structure_id."<br />";
 
-												// Assign category, if specified
-												if ($prefs['feature_categories'] && isset($acc['categoryId'])) {
-													try {
-														$categoryId = intval($acc['categoryId']);
-														if ($categoryId > 0) {
-															// Validate the category before adding it
-															$categlib = TikiLib::lib('categ');
-															$categories = $categlib->get_category($categoryId);
-															if ($categories !== false && !empty($categories)) {
-																$categlib->categorizePage($page, $categoryId, $aux["sender"]["user"]);
-																$content.= "Page: $page categorized. Id: ".$categoryId."<br />";
-															} else {
-																$content.= "Page: $page not categorized. Invalid categoryId: ".$categoryId."<br />";
+													$tikilib->update_page($page, $body, "Updated from " . $acc["account"],
+																			$aux["sender"]["user"],
+																			$options['ip_source'],
+																			'',	//desc
+																			0, 	//edit_minor
+																			'',	//lang
+																			$parsed_data['is_html'],	//is_html
+																			'',	//hash
+																			null,	//saveLastModif
+																			$parsed_data['wysiwyg']	//wysiwyg
+													);
+													$content.= "Page: $page has been updated<br />";
+												} else {
+													
+													// Create a regular page
+													$tikilib->create_page($page, 0, $body, $tikilib->now, "Created from " . $acc["account"], $aux["sender"]["user"],
+														'0.0.0.0',
+														'',						//description
+														'',						//lang
+														$parsed_data['is_html'],	//is_html
+														'',						//hash
+														$parsed_data['wysiwyg']	//wysiwyg
+														);
+													$content.= "Page: $page has been created<br />";
+
+													// Assign category, if specified
+													if ($prefs['feature_categories'] && isset($acc['categoryId'])) {
+														try {
+															$categoryId = intval($acc['categoryId']);
+															if ($categoryId > 0) {
+																// Validate the category before adding it
+																$categlib = TikiLib::lib('categ');
+																$categories = $categlib->get_category($categoryId);
+																if ($categories !== false && !empty($categories)) {
+																	$categlib->categorizePage($page, $categoryId, $aux["sender"]["user"]);
+																	$content.= "Page: $page categorized. Id: ".$categoryId."<br />";
+																} else {
+																	$content.= "Page: $page not categorized. Invalid categoryId: ".$categoryId."<br />";
+																}
 															}
+														} catch (Exception $e) {
+															$content.= "Failed to categorize page: $page  categoryId: ".$categoryId.". Error: ".$e->getMessage()."<br />";
 														}
-													} catch (Exception $e) {
-														$content.= "Failed to categorize page: $page  categoryId: ".$categoryId.". Error: ".$e->getMessage()."<br />";
 													}
 												}
 												
 											} else {
-												$tikilib->update_page($page, $body, "Created from " . $acc["account"],
+												$tikilib->update_page($page, $body, "Updated from " . $acc["account"],
 																		$aux["sender"]["user"],
 																		'0.0.0.0',
 																		'',	//desc
@@ -659,8 +738,9 @@ foreach ($accs['data'] as $acc) {
 										}
 									}
 								} elseif ($acc['type'] == 'wiki-append' || $acc['type'] == 'wiki-prepend' || ($acc['type'] == 'wiki' && $method == "APPEND") || ($acc['type'] == 'wiki' && $method == "PREPEND")) {
-
-									// This is used to UPDATE wiki pages
+									//////////////
+									//	wiki-append, wiki-prepend, wiki APPEND, wiki PREPEND
+									//////////////////////////////////////////////////////////////////////////////////
 
 									// Check permissions
 									$chkUser = $aux["sender"]["user"];
@@ -725,6 +805,9 @@ foreach ($accs['data'] as $acc) {
 										}
 									}
 								} else {
+									//////////////
+									//	Invalid mail-in type
+									//////////////////////////////////////////////////////////////////////////////////
 									$mail = new TikiMail();
 									$mail->setFrom($acc["account"]);
 									$l = $prefs['language'];

commit 29159c844b987f4a3b84f4a137a48c1dfc1c648a

[REF] structures: Remove an unused function that was full of errors (introduced in r46430 but no other reference to it found now, if it was being used i cannot see how it could have worked)

lib/structures/structlib.php
@@ -1381,24 +1381,7 @@ class StructLib extends TikiLib
 			$this->query($query, array($structure_id, $structure_id, $pos+1, $page_ref_id));
 		}
 	}
-	public function move_to_structure_child($parent_ref_id, $structure_id, $first=true)
-	{
-		$query = "update `tiki_structures` set `pos`=`pos`-1 where `pos`>? and `parent_id`=?";
-		$this->query($query, array((int) $page_info["pos"], (int) $parent_ref_id));
-		if ($first) {
-			$query = "update `tiki_structures` set `pos`=`pos`+1 where `parent_id`=?";
-			$this->query($query, array($structure_id));
-			$pos = 1;
-			$query = "update `tiki_structures` set `structure_id`=?, `parent_id`=?, `pos`=? where `page_ref_id`=?";
-			$this->query($query, array($structure_id, $structure_id, $pos+1, $page_ref_id));
-		} else {
-			$query = "select max(`pos`) from `tiki_structures` where `parent_id`=?";
-			$pos = $this->getOne($query, array($structure_id));
-			$query = "update `tiki_structures` set `structure_id`=?, `parent_id`=?, `pos`=? where `page_ref_id`=?";
-			$this->query($query, array($structure_id, $structure_id, $pos+1, $page_ref_id));
-		}
-		return true;
-	}
+
 	/* transform a structure into a menu */
 	public function to_menu($channels, $structure, $sectionLevel=0, $cumul=0, $params=array())
 	{