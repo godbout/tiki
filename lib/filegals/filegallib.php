godbout/tiki

commit 3f3f695b4e2db0bf194ad628400c4f04c8cfe298

[FIX] draw: Include svg data in checksum to allow "drawn on" images to be stored when fgal_allow_duplicates is enabled
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/13.x@52286 b456876b-0849-0410-b77d-98878d47e9d5

lib/filegals/filegallib.php
@@ -43,12 +43,16 @@ class FileGalLib extends TikiLib
 
 	private function get_file_checksum($galleryId, $path, $data)
 	{
-
+		global $prefs;
 		$savedir = $this->get_gallery_save_dir($galleryId);
 
 		if (false !== $savedir) {
 			if ( filesize($savedir . $path) > 0 ) {
-				return md5_file($savedir . $path);
+				if ($prefs['feature_draw'] === 'n' || empty($data)) {
+					return md5_file($savedir . $path);
+				} else {
+					return md5($savedir . $path . $data);	// for svg images with a background
+				}
 			} else {
 				return md5(time());
 			}

commit 8d783a4f633514a5e5a0e1fd02fe8c0946dc94a1

[FIX] filegals: If feature_draw was disabled and fgal_allow_duplicates was not allowed, then uploading new filegal-based avatars was not possible. Regression from way back in r52286 (by me, sorry - thanks marclaporte)

lib/filegals/filegallib.php
@@ -48,15 +48,14 @@ class FileGalLib extends TikiLib
 
 	private function get_file_checksum($galleryId, $path, $data)
 	{
-		global $prefs;
 		$savedir = $this->get_gallery_save_dir($galleryId);
 
 		if (false !== $savedir) {
 			if ( filesize($savedir . $path) > 0 ) {
-				if ($prefs['feature_draw'] === 'n' || empty($data)) {
+				if (empty($data)) {
 					return md5_file($savedir . $path);
 				} else {
-					return md5($savedir . $path . $data);	// for svg images with a background
+					return md5($savedir . $path . $data);	// for svg images with a background or avatars
 				}
 			} else {
 				return md5(time());