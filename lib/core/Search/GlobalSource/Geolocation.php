godbout/tiki

commit 00ebcad59e64d151194cb78c64f20117059f7526

[ENH] search: Add search by distance for Elastic Search. Initial implementation in plugin list only so far, sorting and soring to come, also some tidying up i expect. Example usage: {LIST()}{filter distance="10km" lat="51.5287336" lon="-0.3824611"}{LIST}
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@60381 b456876b-0849-0410-b77d-98878d47e9d5

lib/core/Search/GlobalSource/Geolocation.php
@@ -14,12 +14,14 @@ class Search_GlobalSource_Geolocation implements Search_GlobalSource_Interface
 		}
 
 		$geolib = TikiLib::lib('geo');
+		$coordsArray = (array) $geolib->get_coordinates($objectType, $objectId);
 		$coordinates = $geolib->get_coordinates_string($objectType, $objectId);
 		$alreadyLocated = isset($data['geo_located']) && $data['geo_located'] == 'y';
 
 		return array(
 			'geo_located' => $typeFactory->identifier(($coordinates || $alreadyLocated) ? 'y' : 'n'),
 			'geo_location' => $typeFactory->identifier($coordinates),
+			'geo_point' => $typeFactory->geopoint($coordsArray),
 		);
 	}
 

lib/core/Search/Type/GeoPoint.php
@@ -1,0 +1,26 @@
+<?php
+// (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: MultivaluePlain.php 57970 2016-03-17 20:08:22Z jonnybradley $
+
+class Search_Type_GeoPoint implements Search_Type_Interface
+{
+	private $lat;
+	private $lon;
+
+	function __construct(array $values)
+	{
+		$this->lat = isset($values['lat']) ? (float) $values['lat'] : null;
+		$this->lon = isset($values['lon']) ? (float) $values['lon'] : null;
+	}
+
+	function getValue()
+	{
+		return [
+			'lat' => $this->lat,
+			'lon' => $this->lon,
+		];
+	}
+}

lib/core/Search/Elastic/Index.php
@@ -121,6 +121,11 @@ class Search_Elastic_Index implements Search_Index_Interface, Search_Index_Query
 						"type" => "nested",
 						"dynamic" =>  true,
 					);
+				} elseif ($entry instanceof Search_Type_GeoPoint) {
+					return array(
+						"type" => "geo_point",
+						"index" => "not_analyzed",
+					);
 				} elseif ($entry instanceof Search_Type_DateTime) {
 					return array(
 						"type" => "date",

lib/core/Search/Elastic/QueryBuilder.php
@@ -13,6 +13,7 @@ use Search_Expr_Range as Range;
 use Search_Expr_Initial as Initial;
 use Search_Expr_MoreLikeThis as MoreLikeThis;
 use Search_Expr_ImplicitPhrase as ImplicitPhrase;
+use Search_Expr_Distance as Distance;
 
 class Search_Elastic_QueryBuilder
 {
@@ -165,6 +166,16 @@ class Search_Elastic_QueryBuilder
 					'boost' => $node->getWeight(),
 				),
 			);
+		} elseif ($node instanceof Distance) {
+			return [
+				'geo_distance' => [
+					"distance" => $node->getDistance(),
+					$node->getField() => [
+						"lat" => $node->getLat(),
+						"lon" => $node->getLon(),
+					]
+				]
+			];
 		} else {
 			throw new Exception(tr('Feature not supported.'));
 		}

lib/core/Search/Elastic/TypeFactory.php
@@ -51,6 +51,11 @@ class Search_Elastic_TypeFactory implements Search_Type_Factory_Interface
 		return new Search_Type_Nested($value);
 	}
 
+	function geopoint($value)
+	{
+		return new Search_Type_GeoPoint($value);
+	}
+
 	function sortable($value)
 	{
 		return new Search_Type_PlainShortText($value);

lib/core/Search/Expr/Distance.php
@@ -1,0 +1,91 @@
+<?php
+// (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: Range.php 57971 2016-03-17 20:09:05Z jonnybradley $
+
+class Search_Expr_Distance implements Search_Expr_Interface
+{
+	private $distance;
+	private $lat;
+	private $lon;
+	private $field;
+	private $weight;
+
+	function __construct($distance, $lat, $lon, $field = 'geo_point', $weight = 1.0)
+	{
+		$this->distance = $distance;
+		$this->lat = (float) $lat;
+		$this->lon = (float) $lon;
+		$this->field = $field;
+		$this->weight = (float) $weight;
+	}
+
+	/*
+	 * url for testing tiki-searchindex.php?filter~distance~distance=1000m&filter~distance~lat=51.4139354&filter~distance~lon=-0.2333563
+	 */
+
+	/**
+	 * @return mixed
+	 */
+	public function getDistance()
+	{
+		return $this->distance;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getLat(): float
+	{
+		return $this->lat;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getLon(): float
+	{
+		return $this->lon;
+	}
+
+	function setType($type)
+	{
+	}
+
+	function setField($field = 'geo_point')
+	{
+	}
+
+	function setWeight($weight)
+	{
+		$this->weight = (float) $weight;
+	}
+
+	function getWeight()
+	{
+		return $this->weight;
+	}
+
+	function walk($callback)
+	{
+		return call_user_func($callback, $this, array());
+	}
+
+	function getValue(Search_Type_Factory_Interface $typeFactory)
+	{
+		$type = $this->type;
+		return $typeFactory->$type($this->string);
+	}
+
+	function getField()
+	{
+		return $this->field;
+	}
+
+	function traverse($callback)
+	{
+		return call_user_func($callback, $callback, $this, array());
+	}
+}

lib/core/Search/Lucene/TypeFactory.php
@@ -53,6 +53,12 @@ class Search_Lucene_TypeFactory implements Search_Type_Factory_Interface
 		return null;
 	}
 
+	/* Not supported in Lucene indexes - use elasticsearch */
+	function geopoint($values)
+	{
+		return null;
+	}
+
 	function sortable($value)
 	{
 		return new Search_Type_ShortText($value);

lib/core/Search/MySql/TypeFactory.php
@@ -54,6 +54,12 @@ class Search_MySql_TypeFactory implements Search_Type_Factory_Interface
 		return null;
 	}
 
+	/* Not supported in Lucene indexes - use elasticsearch */
+	function geopoint($values)
+	{
+		return null;
+	}
+
 	function sortable($value)
 	{
 		return new Search_Type_PlainShortText($value);

commit 2e23d57d0ac9d4a89acbbdfadf155767d27fd877

[FIX] search: Indexing non-geo_located objects with lat=null, lon=null prevented them being searchable so instead index the geo_point field itself as null which seems to work, sorry about that - regression from r60381

lib/core/Search/GlobalSource/Geolocation.php
@@ -14,8 +14,18 @@ class Search_GlobalSource_Geolocation implements Search_GlobalSource_Interface
 		}
 
 		$geolib = TikiLib::lib('geo');
-		$coordsArray = (array) $geolib->get_coordinates($objectType, $objectId);
 		$coordinates = $geolib->get_coordinates_string($objectType, $objectId);
+
+		$coordsArray = (array) $geolib->get_coordinates($objectType, $objectId);
+
+		if (isset($coordsArray['lat'], $coordsArray['lon'])) {
+			unset($coordsArray['zoom']);
+			$coordsArray['lat'] = (float) $coordsArray['lat'];
+			$coordsArray['lon'] = (float) $coordsArray['lon'];
+		} else {
+			$coordsArray = null;
+		}
+
 		$alreadyLocated = isset($data['geo_located']) && $data['geo_located'] == 'y';
 
 		return array(

lib/core/Search/Type/GeoPoint.php
@@ -7,20 +7,15 @@
 
 class Search_Type_GeoPoint implements Search_Type_Interface
 {
-	private $lat;
-	private $lon;
+	private $value;
 
-	function __construct(array $values)
+	function __construct($value)
 	{
-		$this->lat = isset($values['lat']) ? (float) $values['lat'] : null;
-		$this->lon = isset($values['lon']) ? (float) $values['lon'] : null;
+		$this->value = $value;
 	}
 
 	function getValue()
 	{
-		return [
-			'lat' => $this->lat,
-			'lon' => $this->lon,
-		];
+		return $this->value;
 	}
 }