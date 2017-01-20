godbout/tiki

commit 791d2d9435232cb6cd83ddebba7681c9aeda6946

[ENH] Micro optimization, mt_rand is faster and provides 'more random' output
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@60943 b456876b-0849-0410-b77d-98878d47e9d5

lib/sheet/include/org/apicnet/io/OOo/OOoUtil.php
@@ -7,7 +7,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 }
 
 
-/*This file is part of J4PHP - Ensembles de propriétés et méthodes permettant le developpment rapide d'application web modulaire
+/*This file is part of J4PHP - Ensembles de propriï¿½tï¿½s et mï¿½thodes permettant le developpment rapide d'application web modulaire
 Copyright (c) 2002-2004 @PICNet
 
 This program is free software; you can redistribute it and/or
@@ -83,7 +83,7 @@ class OOoUtil extends absOOo{
 			
 			$zip->zip($allFiles, $name);
 		} else {
-			$this -> ErrorTracker(4, "Vous devez d'abord créer un document OpenOffice", 'Zip', __FILE__, __LINE__);
+			$this -> ErrorTracker(4, "Vous devez d'abord crï¿½er un document OpenOffice", 'Zip', __FILE__, __LINE__);
 		}
 	}
 	
@@ -93,7 +93,7 @@ class OOoUtil extends absOOo{
 	}
 	
 	function createDirectories(){
-		$this->tmpdir = CACHE_PATH."/OOotmp".rand();
+		$this->tmpdir = CACHE_PATH."/OOotmp".mt_rand();
 		mkdir ($this->tmpdir);
 		
 		for($i = 0; $i < count($this->directories); $i++){

lib/sheet/include/org/apicnet/io/OOo/absOOo.php
@@ -8,7 +8,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 
 
 /*
-This file is part of J4PHP - Ensembles de propriétés et méthodes permettant le developpment rapide d'application web modulaire
+This file is part of J4PHP - Ensembles de propriï¿½tï¿½s et mï¿½thodes permettant le developpment rapide d'application web modulaire
 Copyright (c) 2002-2004 @PICNet
 
 This program is free software; you can redistribute it and/or
@@ -30,7 +30,7 @@ APIC::import("org.apicnet.io.File");
 
 
 /**
- * absOOo, classe d'abstraction de manipulation des fichiers XML, de sauvegarde des documents et de verification des données envoyé au parser OOo
+ * absOOo, classe d'abstraction de manipulation des fichiers XML, de sauvegarde des documents et de verification des donnï¿½es envoyï¿½ au parser OOo
  * 
  * @package 
  * @author diogene
@@ -40,23 +40,23 @@ APIC::import("org.apicnet.io.File");
  **/
 class absOOo extends ErrorManager {
 	
-	var $DIRXML			 = "";						// chemin du répertoire ou se situe le xml instancié
-	var $XMLTYPE 		 = array(					// Tableau des types de document pouvant être généré
+	var $DIRXML			 = "";						// chemin du rï¿½pertoire ou se situe le xml instanciï¿½
+	var $XMLTYPE 		 = array(					// Tableau des types de document pouvant ï¿½tre gï¿½nï¿½rï¿½
 			'Writer',
 			'Calc',
 			'Impress',
 			'Draw');
-	var $MIME			 = array(					// Instance tu type mime des fichiers OpenOffice crée
+	var $MIME			 = array(					// Instance tu type mime des fichiers OpenOffice crï¿½e
 			'Writer' => "vnd.sun.xml.writer",
 			'Calc' => "vnd.sun.xml.calc");
-	var $FILENAME;									// Nom du ficheir xml instanciée
-	var $xml;										// instance correspondant au xml chargé
-	var $ARGDATA		= array(					// Tableau des données pouvant être envoyé au parseur OOo
-		"PageStyle"	=>  array(						// lorsque la donnée a comme valeur TRUE cela signifie qu'elle est obligatoire si la valeur est FALSE elle est facultative et si une valeur est présente alors c'est la valeur par défaut dans le cas ou elle ne serai pas donnée
+	var $FILENAME;									// Nom du ficheir xml instanciï¿½e
+	var $xml;										// instance correspondant au xml chargï¿½
+	var $ARGDATA		= array(					// Tableau des donnï¿½es pouvant ï¿½tre envoyï¿½ au parseur OOo
+		"PageStyle"	=>  array(						// lorsque la donnï¿½e a comme valeur TRUE cela signifie qu'elle est obligatoire si la valeur est FALSE elle est facultative et si une valeur est prï¿½sente alors c'est la valeur par dï¿½faut dans le cas ou elle ne serai pas donnï¿½e
 			"NameStyle"		=> TRUE,				// 
 			"NameStyleSuiv"	=> FALSE,				// Nom du style de page suivant
 			"pageWidth"		=> "20.999",			
-			"pageHeight"	=> "29.699",			// ne serai pas donnée
+			"pageHeight"	=> "29.699",			// ne serai pas donnï¿½e
 			"printOrient"	=> FALSE,				// (portrait|paysage)
 			"marginT"		=> "2",
 			"marginB"		=> "2",
@@ -65,21 +65,21 @@ class absOOo extends ErrorManager {
 			"writingMode"	=> "lr-tb"),
 		"Header"	=> array(
 			"Text"			=> FALSE,
-			"marginB"		=> FALSE,				// Ecart entre l'entête et le corps du document
-			"marginL"		=> FALSE,				// Marge de gauche de l'entête
-			"marginR"		=> FALSE,				// Marge de Droite de l'entête
-			"minHeight"		=> FALSE,				// Hauteur de l'entête
-			"align"			=> FALSE,				// Alignement du texte de l'entête (left|center|right|justify)
-			"BgColor"		=> FALSE,				// Couleur de fond de l'entête dans le cas ou ce dernier n'a pas d'image #color en hexa
+			"marginB"		=> FALSE,				// Ecart entre l'entï¿½te et le corps du document
+			"marginL"		=> FALSE,				// Marge de gauche de l'entï¿½te
+			"marginR"		=> FALSE,				// Marge de Droite de l'entï¿½te
+			"minHeight"		=> FALSE,				// Hauteur de l'entï¿½te
+			"align"			=> FALSE,				// Alignement du texte de l'entï¿½te (left|center|right|justify)
+			"BgColor"		=> FALSE,				// Couleur de fond de l'entï¿½te dans le cas ou ce dernier n'a pas d'image #color en hexa
 			"color"			=> FALSE),				// Couleur du text
 		"Footer"	=> array(
 			"Text"			=> FALSE,
-			"marginB"		=> FALSE,				// Ecart entre l'entête et le corp du document
-			"marginL"		=> FALSE,				// Marge de gauche de l'entête
-			"marginR"		=> FALSE,				// Marge de Droite de l'entête
-			"minHeight"		=> FALSE,				// Hauteur de l'entête
-			"align"			=> FALSE,				// Alignement du texte de l'entête (left|center|right|justify)
-			"BgColor"		=> FALSE,				// Couleur de fond de l'entête dans le cas ou ce dernier n'a pas d'image #color en hexa
+			"marginB"		=> FALSE,				// Ecart entre l'entï¿½te et le corp du document
+			"marginL"		=> FALSE,				// Marge de gauche de l'entï¿½te
+			"marginR"		=> FALSE,				// Marge de Droite de l'entï¿½te
+			"minHeight"		=> FALSE,				// Hauteur de l'entï¿½te
+			"align"			=> FALSE,				// Alignement du texte de l'entï¿½te (left|center|right|justify)
+			"BgColor"		=> FALSE,				// Couleur de fond de l'entï¿½te dans le cas ou ce dernier n'a pas d'image #color en hexa
 			"color"			=> FALSE),				// Couleur du text
 		"img"		=> array(
 			"src"			=> TRUE,
@@ -111,7 +111,7 @@ class absOOo extends ErrorManager {
 			"underline"		=> FALSE,				// true
 			"bgColor"		=> FALSE,				// true
 			"color"			=> FALSE,				// true
-			"fontName"		=> FALSE,				// Le nom d'une police de caractère
+			"fontName"		=> FALSE,				// Le nom d'une police de caractï¿½re
 			"fontSize"		=> FALSE),				// integer
 		"tableStyle"	=> array(
 			"marginL"		=> FALSE,				// double
@@ -170,7 +170,7 @@ class absOOo extends ErrorManager {
 		);
 	
 	/**
-	 * absOOo::absOOo(), constructeur permettant uniquement d'instancié la classe pere de gestion des erreurs
+	 * absOOo::absOOo(), constructeur permettant uniquement d'instanciï¿½ la classe pere de gestion des erreurs
 	 * 
 	 * @return none
 	 * @access public
@@ -181,10 +181,10 @@ class absOOo extends ErrorManager {
 
 	
 	/**
-	 * absOOo::decode_text(), methode pour décode un text utf8 en texte local avec les caractères accentués et les sigles
+	 * absOOo::decode_text(), methode pour dï¿½code un text utf8 en texte local avec les caractï¿½res accentuï¿½s et les sigles
 	 * 
-	 * @param String $str chaine de caractère a coder
-	 * @return String la chaine de caractère decodé
+	 * @param String $str chaine de caractï¿½re a coder
+	 * @return String la chaine de caractï¿½re decodï¿½
 	 * @access public
 	 **/
 	function decode_text($str){
@@ -192,10 +192,10 @@ class absOOo extends ErrorManager {
 	}
 	
 	/**
-	 * absOOo::encode_text(), methode pour encoder un texte locale en texte utf8 (norme internationnal de codage des caractères)
+	 * absOOo::encode_text(), methode pour encoder un texte locale en texte utf8 (norme internationnal de codage des caractï¿½res)
 	 * 
-	 * @param String $str chaine de caractère a encoder
-	 * @return String la chaine de caractère encoder
+	 * @param String $str chaine de caractï¿½re a encoder
+	 * @return String la chaine de caractï¿½re encoder
 	 * @access public
 	 **/
 	function encode_text($str){
@@ -230,10 +230,10 @@ class absOOo extends ErrorManager {
 	}
 	
 	/**
-	 * absOOo::verifIntegrite(), vérifie l'intégrité des données pour la génération du document OOo
+	 * absOOo::verifIntegrite(), vï¿½rifie l'intï¿½gritï¿½ des donnï¿½es pour la gï¿½nï¿½ration du document OOo
 	 * 
-	 * @param array $arrayData le tableau de données a vérifier
-	 * @param String $typeArray le type du tableau de données. les type de données sont : "PageStyle", "Header", "Footer", "img", "paraStyle", "textStyle", "styleCarac", "tableStyle", 
+	 * @param array $arrayData le tableau de donnï¿½es a vï¿½rifier
+	 * @param String $typeArray le type du tableau de donnï¿½es. les type de donnï¿½es sont : "PageStyle", "Header", "Footer", "img", "paraStyle", "textStyle", "styleCarac", "tableStyle", 
 	 * @return none
 	 * @access private
 	 **/
@@ -299,7 +299,7 @@ class absOOo extends ErrorManager {
 			    $backgroundImageNode =& $this->xml->createElement("style:background-image");
 				
 				$ext = substr($style["img"]["src"], strlen($file)-3);
-				$tmpfile = rand().".".$ext;
+				$tmpfile = mt_rand().".".$ext;
 				copy($style["img"]["src"], $dir."/Pictures/".$tmpfile);
 				
 				$backgroundImageNode->setAttribute("xlink:href", "#Pictures/".$tmpfile);
@@ -354,15 +354,15 @@ class absOOo extends ErrorManager {
 			
 			return $tabsNode;
 		} else {
-			$this -> ErrorTracker(4, "Les tabulation sont mal définies", 'setTabs', __FILE__, __LINE__);
+			$this -> ErrorTracker(4, "Les tabulation sont mal dï¿½finies", 'setTabs', __FILE__, __LINE__);
 		}
 	}
 	
 	/**
-	 * absOOo::accessor(), méthode de recherche d'un enfant. cette méthode ne renvoit pas un pointeur sur l'enfant mais une copie de l'enfant
+	 * absOOo::accessor(), mï¿½thode de recherche d'un enfant. cette mï¿½thode ne renvoit pas un pointeur sur l'enfant mais une copie de l'enfant
 	 * 
-	 * @param String $path le chemin du noeud recherché
-	 * @param integer $item la position de l'enfant recherché
+	 * @param String $path le chemin du noeud recherchï¿½
+	 * @param integer $item la position de l'enfant recherchï¿½
 	 * @return DOMIT_Nodes l'enfant rechercher
 	 * @access private
 	 **/
@@ -600,9 +600,9 @@ class absOOo extends ErrorManager {
 	/**
 	 * absOOo::ChildText()
 	 * 
-	 * @param String $tagName, nom de l'élément (balise) à créer
-	 * @param String $text, valeur de la balise créée
-	 * @return DOMIT_Node le nouveau noeud crée
+	 * @param String $tagName, nom de l'ï¿½lï¿½ment (balise) ï¿½ crï¿½er
+	 * @param String $text, valeur de la balise crï¿½ï¿½e
+	 * @return DOMIT_Node le nouveau noeud crï¿½e
 	 * @access private
 	 **/
 	function ChildText($tagName, $text){
@@ -614,7 +614,7 @@ class absOOo extends ErrorManager {
 	
 	
 	/**
-	 * absOOo::toString(), convertir l'arbre xml en chaine de caractère.
+	 * absOOo::toString(), convertir l'arbre xml en chaine de caractï¿½re.
 	 * 
 	 * @return String 
 	 * @access private

lib/sheet/include/org/apicnet/io/OOo/objOOo/OOoImg.php
@@ -8,7 +8,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 }
 
 
-/*This file is part of J4PHP - Ensembles de propriétés et méthodes permettant le developpment rapide d'application web modulaire
+/*This file is part of J4PHP - Ensembles de propriï¿½tï¿½s et mï¿½thodes permettant le developpment rapide d'application web modulaire
 Copyright (c) 2002-2004 @PICNet
 
 This program is free software; you can redistribute it and/or
@@ -116,7 +116,7 @@ class OOoImg extends absOOo {
 		$imageNode->setAttribute("draw:name", $name);
 		
 		$ext = substr($this->_styleImg["src"], strlen($file)-3);
-		$tmpfile = rand().".".$ext;
+		$tmpfile = mt_rand().".".$ext;
 		copy($this->_styleImg["src"], $dir."/Pictures/".$tmpfile);
 		if (isset($this->_styleImg["src"])) $imageNode->setAttribute("xlink:href", "#Pictures/".$tmpfile);
 		if (isset($this->_styleImg["anchorType"])) $imageNode->setAttribute("text:anchor-type", $this->_styleImg["anchorType"]);

lib/sheet/include/org/apicnet/io/OOo/objOOo/OOoTable.php
@@ -7,7 +7,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 }
 
 
-/*This file is part of J4PHP - Ensembles de propriétés et méthodes permettant le developpment rapide d'application web modulaire
+/*This file is part of J4PHP - Ensembles de propriï¿½tï¿½s et mï¿½thodes permettant le developpment rapide d'application web modulaire
 Copyright (c) 2002-2004 @PICNet
 
 This program is free software; you can redistribute it and/or
@@ -88,7 +88,7 @@ class OOoTable extends absOOo {
 	 * @return array
 	 **/
 	function colMaxNextSpan($argArray, $curentCol){
-		$max    = $curentCol*1000000;	// un nombre très grand
+		$max    = $curentCol*1000000;	// un nombre trï¿½s grand
 		
 		if (count($argArray) > 1) {
 		    for(reset($argArray); $key = key($argArray); next($argArray)) {
@@ -108,7 +108,7 @@ class OOoTable extends absOOo {
 	
 	
 	/**
-	 * OOoTable::colSpan(), méthode de fusion de colonnes. Le numéro des colonnes commence à 0.
+	 * OOoTable::colSpan(), mï¿½thode de fusion de colonnes. Le numï¿½ro des colonnes commence ï¿½ 0.
 	 * 
 	 * @param $argSpan = array(
 	 			"lignSPan"	=> 1,
@@ -142,7 +142,7 @@ class OOoTable extends absOOo {
 	
 	
 	/**
-	 * OOoTable::rowSpan(), méthode de fusion de lignes. Le numéro des lignes commence à 0.
+	 * OOoTable::rowSpan(), mï¿½thode de fusion de lignes. Le numï¿½ro des lignes commence ï¿½ 0.
 	 * 
 	 * @param $argSpan = array(
 	 			"lignDeb"	=> 1,
@@ -177,7 +177,7 @@ class OOoTable extends absOOo {
 	}
 
     /**
-     * table::addcellData(), méthode d'ajout de données dans une cellule.
+     * table::addcellData(), mï¿½thode d'ajout de donnï¿½es dans une cellule.
      * 
      * @param  $argData  = array(
 	 			"data"			=> DATA
@@ -208,7 +208,7 @@ class OOoTable extends absOOo {
 			    $this->_ligne[$row][$column] = $argData;
 			} 
 		} else {
-			$this -> ErrorTracker(4, "argData doit être un tableau", 'colSpan', __FILE__, __LINE__);
+			$this -> ErrorTracker(4, "argData doit ï¿½tre un tableau", 'colSpan', __FILE__, __LINE__);
 		}
     }
 	
@@ -244,7 +244,7 @@ class OOoTable extends absOOo {
 		    $backgroundImageNode =& $this->xml->createElement("style:background-image");
 				
 			$ext = substr($this->_style["img"]["src"], strlen($file)-3);
-			$tmpfile = rand().".".$ext;
+			$tmpfile = mt_rand().".".$ext;
 			copy($this->_style["img"]["src"], $dir."/Pictures/".$tmpfile);
 			
 			$backgroundImageNode->setAttribute("xlink:href", "#Pictures/".$tmpfile);
@@ -262,13 +262,13 @@ class OOoTable extends absOOo {
 		$styleNode->appendChild($propertiesNode);
 		$nodeStyle->appendChild($styleNode);
 		
-		/*********************Création du tableau**********************/
+		/*********************Crï¿½ation du tableau**********************/
 		$tableNode =& $this->xml->createElement("table:table");
 		$tableNode->setAttribute("table:name", $StyleName);
 		$tableNode->setAttribute("table:style-name", $StyleName);
 		
 		$nodeContent->appendChild($tableNode);
-		/**********************Fin de Création*************************/
+		/**********************Fin de Crï¿½ation*************************/
 		
 		$rowSpanNb = 0;
 		
@@ -282,7 +282,7 @@ class OOoTable extends absOOo {
 
 			for($j = 0; $j < $this->_column; $j++){
 				
-				/**********************Création du style de la colonne*********************************/
+				/**********************Crï¿½ation du style de la colonne*********************************/
 	  			if (isset($this -> _style["widthRow"][$j]) && $i === 0) {
 		  			$StyleNameR = $StyleName.".R".$STYLNUM['table_row'];
 					$STYLNUM['table_row']++;
@@ -306,9 +306,9 @@ class OOoTable extends absOOo {
 						$tableNode->appendChild($tableColumnNode);
 					}
 				}
-				/*********************Fin de la Création du style de la colonne**************************/
+				/*********************Fin de la Crï¿½ation du style de la colonne**************************/
 			   
-				/*****************************Création du style de la cellule************************/
+				/*****************************Crï¿½ation du style de la cellule************************/
 				$StyleNameC = $StyleName.".C".$STYLNUM['table_cel'];
 				$STYLNUM['table_cel']++;
 				$styleNode =& $this->xml->createElement("style:style");
@@ -332,7 +332,7 @@ class OOoTable extends absOOo {
 				    $backgroundImageNode =& $this->xml->createElement("style:background-image");
 					
 					$ext = substr($this->_ligne[$i][$j]["img"]["src"], strlen($file)-3);
-					$tmpfile = rand().".".$ext;
+					$tmpfile = mt_rand().".".$ext;
 					copy($this->_ligne[$i][$j]["img"]["src"], $dir."/Pictures/".$tmpfile);
 					
 					$backgroundImageNode->setAttribute("xlink:href", "#Pictures/".$tmpfile);
@@ -349,9 +349,9 @@ class OOoTable extends absOOo {
 				
 				$styleNode->appendChild($propertiesNode);
 				$nodeStyle->appendChild($styleNode);
-				/**********************Fin de Création*************************/
+				/**********************Fin de Crï¿½ation*************************/
 				
-				/********************Création de la cellule********************/
+				/********************Crï¿½ation de la cellule********************/
 			/*	"lignDeb"		=> TRUE,
 				"colDeb"		=> TRUE,
 				"nbLign"		=> TRUE,
@@ -404,7 +404,7 @@ class OOoTable extends absOOo {
 				}
 				
 				$tableRowNode->appendChild($tableCellNode);
-				/**********************Fin de Création*************************/
+				/**********************Fin de Crï¿½ation*************************/
 			}
 			
 			if ($i === 0) $tableNode->appendChild($tableHeaderRowsNode);

lib/bigbluebuttonlib.php
@@ -147,7 +147,7 @@ class BigBlueButtonLib
 		if ( isset($params['voicebridge']) ) {
 			$request['voiceBridge'] = $params['voicebridge'];
 		} else {
-			$request['voiceBridge'] = '7' . rand(0, 9999);
+			$request['voiceBridge'] = '7' . mt_rand(0, 9999);
 		}
 
 		if ( isset($params['logout']) ) {

lib/core/Math/Formula/Function/Random.php
@@ -15,7 +15,7 @@ class Math_Formula_Function_Random extends Math_Formula_Function
 			$range[] = $this->evaluateChild($child);
 		}
 
-		return rand(min($range), max($range));
+		return mt_rand(min($range), max($range));
 	}
 }
 

lib/core/Services/Connect/Server.php
@@ -30,7 +30,7 @@ class Services_Connect_Server
 		$capkey = $caplib->generate();
 
 		$status = 'pending';
-		$guid = uniqid(rand(), true);
+		$guid = uniqid(mt_rand(), true);
 
 		$captcha = strip_tags($caplib->render());
 
@@ -118,7 +118,7 @@ class Services_Connect_Server
 				$this->connectlib->recordConnection($status, $guid, $message, true);
 				$rdata = array(
 					'status' => $status,
-					'newguid' => uniqid(rand(), true),
+					'newguid' => uniqid(mt_rand(), true),
 					'message' => $message,
 				);
 			}

lib/dcs/dcslib.php
@@ -212,7 +212,7 @@ class DCSLib extends TikiLib
 			return '';
 		}
 
-		$x = rand(0, $cant - 1);
+		$x = mt_rand(0, $cant - 1);
 		$query = 'SELECT * FROM `tiki_programmed_content`' . $where;
 		$result = $this->fetchAll($query, $bindvars, 1, $x);
 

commit 31368239e1b0c16ca15a50735f21ea099c1ac297

[FIX] Character encoding issues introduced in r60943 is now restored

lib/sheet/include/org/apicnet/io/OOo/OOoUtil.php
@@ -93,7 +93,7 @@ class OOoUtil extends absOOo{
 	}
 	
 	function createDirectories(){
-		$this->tmpdir = CACHE_PATH."/OOotmp".mt_rand();
+		$this->tmpdir = CACHE_PATH."/OOotmp".rand();
 		mkdir ($this->tmpdir);
 		
 		for($i = 0; $i < count($this->directories); $i++){

lib/sheet/include/org/apicnet/io/OOo/absOOo.php
@@ -299,7 +299,7 @@ class absOOo extends ErrorManager {
 			    $backgroundImageNode =& $this->xml->createElement("style:background-image");
 				
 				$ext = substr($style["img"]["src"], strlen($file)-3);
-				$tmpfile = mt_rand().".".$ext;
+				$tmpfile = rand().".".$ext;
 				copy($style["img"]["src"], $dir."/Pictures/".$tmpfile);
 				
 				$backgroundImageNode->setAttribute("xlink:href", "#Pictures/".$tmpfile);

lib/sheet/include/org/apicnet/io/OOo/objOOo/OOoImg.php
@@ -116,7 +116,7 @@ class OOoImg extends absOOo {
 		$imageNode->setAttribute("draw:name", $name);
 		
 		$ext = substr($this->_styleImg["src"], strlen($file)-3);
-		$tmpfile = mt_rand().".".$ext;
+		$tmpfile = rand().".".$ext;
 		copy($this->_styleImg["src"], $dir."/Pictures/".$tmpfile);
 		if (isset($this->_styleImg["src"])) $imageNode->setAttribute("xlink:href", "#Pictures/".$tmpfile);
 		if (isset($this->_styleImg["anchorType"])) $imageNode->setAttribute("text:anchor-type", $this->_styleImg["anchorType"]);

lib/sheet/include/org/apicnet/io/OOo/objOOo/OOoTable.php
@@ -244,7 +244,7 @@ class OOoTable extends absOOo {
 		    $backgroundImageNode =& $this->xml->createElement("style:background-image");
 				
 			$ext = substr($this->_style["img"]["src"], strlen($file)-3);
-			$tmpfile = mt_rand().".".$ext;
+			$tmpfile = rand().".".$ext;
 			copy($this->_style["img"]["src"], $dir."/Pictures/".$tmpfile);
 			
 			$backgroundImageNode->setAttribute("xlink:href", "#Pictures/".$tmpfile);
@@ -332,7 +332,7 @@ class OOoTable extends absOOo {
 				    $backgroundImageNode =& $this->xml->createElement("style:background-image");
 					
 					$ext = substr($this->_ligne[$i][$j]["img"]["src"], strlen($file)-3);
-					$tmpfile = mt_rand().".".$ext;
+					$tmpfile = rand().".".$ext;
 					copy($this->_ligne[$i][$j]["img"]["src"], $dir."/Pictures/".$tmpfile);
 					
 					$backgroundImageNode->setAttribute("xlink:href", "#Pictures/".$tmpfile);