godbout/tiki

commit d444304f14b76efa210c0147859d51d432ed1df8

[FIX] check javascript enable
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15210 b456876b-0849-0410-b77d-98878d47e9d5

templates/error_ticket.tpl
@@ -33,8 +33,8 @@
 {/foreach}
 <input type="submit" name="ticket_action_button" value="{tr}Click here to confirm your action{/tr}" />
 </form><br /><br />
-        <a href="javascript:history.back()" class="linkmenu">{tr}Go back{/tr}</a><br />
-        <a href="{$prefs.tikiIndex}" class="linkmenu">{tr}Return to home page{/tr}</a>
+        {if $prefs.javascript_enabled eq 'y'}{button href="javascript:history.back()" _text="{tr}Go back{/tr}"}<br /><br />{/if}
+        {button href="$prefs.tikiIndex" _text="{tr}Return to home page{/tr}"}
         </div>
         </div>
       </div></td>

templates/error.tpl
@@ -63,7 +63,7 @@ close();
 					{/if}
 				{/if}
 				{if $page and $create eq 'y' and ($tiki_p_admin eq 'y' or $tiki_p_admin_wiki eq 'y' or $tiki_p_edit eq 'y')}{button href="tiki-editpage.php?page=$page" _text="{tr}Create this page{/tr}"} {tr}(page will be orphaned){/tr}<br /><br />{/if}
-				{button href="javascript:history.back()" _text="{tr}Go back{/tr}"}<br /><br />
+				{if $prefs.javascript_enabled eq 'y'}{button href="javascript:history.back()" _text="{tr}Go back{/tr}"}<br /><br />{/if}
 				{button href="$prefs.tikiIndex" _text="{tr}Return to home page{/tr}"}
 			</div>
 		</div>

commit 10f2cb0d87aa1e3014e764b46e92622cee340fc4

Remove button "Go back" and "Return to home page", broken in r15210

templates/error_ticket.tpl
@@ -28,10 +28,6 @@
 							<input type="submit" class="btn btn-success" name="ticket_action_button"
 								value="{tr}Click here to confirm your action{/tr}">
 						</form>
-						{if $prefs.javascript_enabled eq 'y'}
-							{button href="javascript:history.back()" _text="{tr}Go back{/tr}"}
-						{/if}
-						{button href="$prefs.tikiIndex" _text="{tr}Return to home page{/tr}"}
 					</div>
 				</div>
 			</div>
@@ -50,10 +46,6 @@
 							<input type="submit" class="btn btn-success" name="ticket_action_button"
 								value="{tr}Click here to confirm your action{/tr}">
 						</form>
-						{if $prefs.javascript_enabled eq 'y'}
-							{button href="javascript:history.back()" _text="{tr}Go back{/tr}"}
-						{/if}
-						{button href="$prefs.tikiIndex" _text="{tr}Return to home page{/tr}"}
 					</div>
 				</div>
 			</div>
@@ -75,10 +67,6 @@
 							<input type="submit" class="btn btn-success" name="ticket_action_button"
 								value="{tr}Click here to confirm your action{/tr}">
 						</form>
-						{if $prefs.javascript_enabled eq 'y'}
-							{button href="javascript:history.back()" _text="{tr}Go back{/tr}"}
-						{/if}
-						{button href="$prefs.tikiIndex" _text="{tr}Return to home page{/tr}"}
 					</div>
 				</div>
 			</div>
@@ -100,10 +88,6 @@
 							<input type="submit" class="btn btn-success" name="ticket_action_button"
 								value="{tr}Click here to confirm your action{/tr}">
 						</form>
-						{if $prefs.javascript_enabled eq 'y'}
-							{button href="javascript:history.back()" _text="{tr}Go back{/tr}"}
-						{/if}
-						{button href="$prefs.tikiIndex" _text="{tr}Return to home page{/tr}"}
 					</div>
 				</div>
 			</div>