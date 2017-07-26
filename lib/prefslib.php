godbout/tiki

commit 3d6c227c2abf645e08a69da03b1d67a431f3d423

[NEW] Features get the ability to have a profile as a dependency by setting an array with the parent key in lib/prefs/feature 'dependencies' of 'profiles'
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@42094 b456876b-0849-0410-b77d-98878d47e9d5

lib/prefslib.php
@@ -426,7 +426,7 @@ class PreferencesLib
 	{
 		$out = array();
 
-		foreach ( (array) $dependencies as $dep ) {
+		foreach ( (array) $dependencies as $key => $dep ) {
 			$info = $this->getPreference($dep, false);
 			if ( $info ) {
 				$out[] = array(
@@ -438,6 +438,18 @@ class PreferencesLib
 						( $info['type'] == 'flag' && $info['value'] == 'y' )
 						|| ( $info['type'] != 'flag' && ! empty( $info['value'] ) )
 				);
+			} elseif ($key == 'profiles') {
+				foreach($dep as $profile) {
+					$out[] = array(
+						'name' => $profile,
+						'label' => $profile,
+						'type' => 'profile',
+						'link' => 'tiki-admin.php?page=profiles&list=List&profile=' . urlencode($profile),
+						'met' =>
+						( $info['type'] == 'flag' && $info['value'] == 'y' )
+							|| ( $info['type'] != 'flag' && ! empty( $info['value'] ) )
+					);
+				}
 			}
 		}
 

templates/prefs/shared-dependencies.tpl
@@ -3,6 +3,8 @@
 	{foreach from=$p.dependencies item=dep}
 		{if $dep.met}
 			{icon _id="accept" class="pref_dependency tips" title="{tr}Requires:{/tr} "|cat:$dep.label|escape|cat:" (OK)"}
+		{elseif $dep.type eq 'profile'}
+			<div class="pref_dependency highlight">{tr}You need apply profile{/tr} <a href="{$dep.link|escape}">{$dep.label|escape}</a></div>
 		{else}
 			<div class="pref_dependency highlight">{tr}You need to set{/tr} <a href="{$dep.link|escape}">{$dep.label|escape}</a></div>
 		{/if}

commit 6948e00f0d47e95e45d85bb91fd8fcf060011cb2

[REF] getDependencies(): Flag issue introduced in r42094

lib/prefslib.php
@@ -507,7 +507,7 @@ class PreferencesLib
 						'label' => $profile,
 						'type' => 'profile',
 						'link' => 'tiki-admin.php?page=profiles&list=List&profile=' . urlencode($profile),
-						'met' =>
+						'met' => // FIXME: $info is false, the following surely won't behave as intended. This should indicate whether the profile was applied.
 						( $info['type'] == 'flag' && $info['value'] == 'y' )
 							|| ( $info['type'] != 'flag' && ! empty( $info['value'] ) )
 					);