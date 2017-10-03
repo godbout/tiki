godbout/tiki

commit b67d750a59761472c2d13fdb5908261e475563cf

[REF] Installer: Replace confusing $patches and $installed properties by Patch class Implement the Singleton pattern (not yet enforced)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@63441 b456876b-0849-0410-b77d-98878d47e9d5

lib/core/Tiki/Command/UpdateCommand.php
@@ -37,10 +37,8 @@ class UpdateCommand extends Command
 		if ($installed) {
 			$installer->update();
 			$output->writeln('Update completed.');
-			if (count($installer->installed)) {
-				foreach ($installer->installed as $patch) {
-					$output->writeln("<info>Installed: $patch</info>");
-				}
+			foreach (array_keys(\Patch::getPatches([\Patch::NEWLY_APPLIED])) as $patch) {
+				$output->writeln("<info>Installed: $patch</info>");
 			}
 
 			if ( count($installer->executed) ) {

installer/Patch.php
@@ -1,0 +1,74 @@
+<?php
+// (c) Copyright 2002-2017 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+/**
+ * A procedure to adapt a Tiki database to a code change, implemented by SQL, Yaml or PHP files
+ * @see Installer
+ */
+class Patch
+{
+	static $list = null;
+	const NOT_APPLIED = 0;
+	const ALREADY_APPLIED = 1;
+	const NEWLY_APPLIED = 2;
+
+	private $name;
+	private $status = null;
+
+	function __construct($name, $status)
+	{
+		$this->name = $name;
+		$this->status = $status;
+	}
+
+	/**
+	 * Defines the state
+	 * @param int $status One of the constants of this class
+	 */
+	public function setStatus($status)
+	{
+		if (! in_array($status, [self::NOT_APPLIED, self::ALREADY_APPLIED, self::NEWLY_APPLIED])) {
+			throw new DomainException();
+		}
+		$this->status = $status;
+	}
+
+
+	/**
+	 * Get the patches matching the specified statuses
+	 * @param int[] $statuses Allowed statuses
+	 * @return Patch[] Matching patches
+	 */
+	static function getPatches($statuses)
+	{
+		$matches = [];
+		foreach (self::$list as $name => $patch) {
+			if (in_array($patch->status, $statuses)) {
+				$matches[$name] = $patch;
+			}
+		}
+		return $matches;
+	}
+
+	/**
+	 * Indicates if the patch is applied
+	 * @return bool true is the patch is applied, false otherwise
+	 */
+	public function isApplied()
+	{
+		return $this->status != self::NOT_APPLIED;
+	}
+
+	/**
+	 * Mark as installed
+	 */
+	function record()
+	{
+		Installer::getInstance()->query("INSERT INTO tiki_schema (patch_name, install_date) VALUES(?, NOW())", array($this->name));
+		self::$list[$this->name]->setStatus(self::NEWLY_APPLIED);
+	}
+}

installer/installlib.php
@@ -13,16 +13,16 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 
 
 require_once 'lib/setup/twversion.class.php';
+require_once 'Patch.php';
 
 /**
- *
+ * @see Patch
  */
 class Installer extends TikiDb_Bridge
 {
-	public $patches = array();
-	public $scripts = array();
+	static $instance = null; // Singleton instance
 
-	public $installed = array();
+	public $scripts = array();
 	public $executed = array();
 
 	public $queries = array('successful' => [], 'failed' => []);
@@ -30,13 +30,24 @@ class Installer extends TikiDb_Bridge
 	public $useInnoDB = false;
 
     /**
-     *
+     * TODO: make private to enforce Singleton
      */
     function __construct() // {{{
 	{
 		$this->buildPatchList();
 		$this->buildScriptList();
 	} // }}}
+
+	/**
+	 * Get the instance (creating one if necessary)
+	 * @return Installer
+	 */
+	static function getInstance() {
+		if (is_null(self::$instance)) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
 
 	function cleanInstall() // {{{
 	{
@@ -59,10 +70,9 @@ class Installer extends TikiDb_Bridge
 			$this->buildScriptList();
 
 			// Base SQL file contains the distribution tiki patches up to this point
-			$patches = $this->patches;
-			foreach ( $patches as $patch ) {
-				if ( preg_match('/_tiki$/', $patch) ) {
-					$this->recordPatch($patch);
+			foreach ( Patch::$list as $patchName => $patch ) {
+				if ( preg_match('/_tiki$/', $patchName) ) {
+					$patch->record();
 				}
 			}
 		}
@@ -115,9 +125,10 @@ class Installer extends TikiDb_Bridge
 			$this->runFile($secdb);
 		}
 		
-		$patches = $this->patches;
-		foreach ($patches as $patch) {
-			$this->installPatch($patch);
+		foreach (Patch::$list as $patchName => $patch) {
+			if (! $patch->isApplied()) {
+				$this->installPatch($patchName);
+			}
 		}
 
 		foreach ( $this->scripts as $script ) {
@@ -130,7 +141,7 @@ class Installer extends TikiDb_Bridge
      */
     function installPatch( $patch ) // {{{
 	{
-		if ( ! in_array($patch, $this->patches) ) {
+		if ( Patch::$list[$patch]->isApplied() ) {
 			return;
 		}
 
@@ -172,8 +183,7 @@ class Installer extends TikiDb_Bridge
 		}
 
 		if (!isset($status) || $status ) {
-			$this->installed[] = $patch;
-			$this->recordPatch($patch);
+			Patch::$list[$patch]->record();
 		}
 	} // }}}
 
@@ -194,14 +204,6 @@ class Installer extends TikiDb_Bridge
 		$this->executed[] = $script;
 	} // }}}
 
-    /**
-     * @param $patch
-     */
-    function recordPatch( $patch ) // {{{
-	{
-		$this->query("INSERT INTO tiki_schema (patch_name, install_date) VALUES(?, NOW())", array($patch));
-		$this->patches = array_diff($this->patches, array($patch));
-	} // }}}
 
 	private function applyProfile($profileFile)
 	{
@@ -309,35 +311,48 @@ class Installer extends TikiDb_Bridge
 	/**
 	 * @throws Exception In case of filesystem access issue
 	 */
-	function buildPatchList() // {{{
+	function buildPatchList()
 	{
-		$this->patches = array();
+		// Optimization
+		if (! is_null(Patch::$list)) {
+			return;
+		}
 
+		$patches = [];
 		$files = glob(dirname(__FILE__) . '/schema/*_*.{sql,yml,php}', GLOB_BRACE); // "php" for standalone PHP scripts
 		if ($files === false) {
 			throw new Exception('Failed to scan patches');
 		}
 		foreach ( $files as $file ) {
 			$filename = basename($file);
-			$this->patches[] = substr($filename, 0, -4);
+			$patches[] = substr($filename, 0, -4);
 		}
-		$this->patches = array_unique($this->patches);
+		$patches = array_unique($patches);
 
 		$installed = array();
 
 		if ($this->tableExists('tiki_schema')) {
 			$installed = $this->table('tiki_schema')->fetchColumn('patch_name', array());
 		}
-		
+
 		if ( empty($installed) ) {
 			// Erase initial error
 			$this->queries['failed'] = array();
 		}
 
-		$this->patches = array_diff($this->patches, $installed);
+		Patch::$list = array();
+		sort($patches);
+		foreach ($patches as $patchName) {
+			if (in_array($patchName, $installed)) {
+				$status = Patch::ALREADY_APPLIED;
+			} else {
+				$status = Patch::NOT_APPLIED;
+			}
+			$patch = new Patch($patchName, $status);
+			Patch::$list[$patchName] = $patch;
+		}
+	}
 
-		sort($this->patches);
-	} // }}}
 
 	function buildScriptList() // {{{
 	{
@@ -372,7 +387,7 @@ class Installer extends TikiDb_Bridge
      */
     function requiresUpdate() // {{{
 	{
-		return count($this->patches) > 0 ;
+		return count(Patch::getPatches([Patch::NOT_APPLIED])) > 0;
 	} // }}}
  function checkInstallerLocked() // {{{
 	{

installer/tiki-installer.php
@@ -846,7 +846,7 @@ if ( file_exists($local) ) {
 		if ( $dbcon = initTikiDB($api_tiki, $db_tiki, $host_tiki, $user_tiki, $pass_tiki, $dbs_tiki, $client_charset, $dbTiki) ) {
 			$smarty->assign('resetdb', isset($_POST['reset']) ? 'y' : 'n');
 
-			$installer = new Installer;
+			$installer = Installer::getInstance();
 			$installer->setServerType($db_tiki);
 
 			if ( ! $client_charset_forced ) {
@@ -858,7 +858,7 @@ if ( file_exists($local) ) {
 		}
 	}
 } elseif ($dbcon) {
-	$installer = new Installer;
+	$installer = Installer::getInstance();
 	TikiDb::get()->setErrorHandler(new InstallerDatabaseErrorHandler);
 } else {
 	// If there is no local.php we check if there is a db/preconfiguration.php preconfiguration file with database connection values which we can prefill the installer with
@@ -942,7 +942,7 @@ if (
 			include $local;
 			// In case of replication, ignore it during installer.
 			unset($shadow_dbs, $shadow_user, $shadow_pass, $shadow_host);
-			$installer = new Installer;
+			$installer = Installer::getInstance();
 			$installer->setServerType($db_tiki);
 		}
 	}
@@ -1215,8 +1215,7 @@ if ($install_step == '2') {
 	$smarty->assign('gd_test', $gd_test);
 } elseif ($install_step == 6 && !empty($_POST['validPatches'])) {
 	foreach ($_POST['validPatches'] as $patch) {
-		global $installer;
-		$installer->recordPatch($patch);
+		Patch::$list[$patch]->record();
 	}
 }
 

commit ce7732e37f9f5ee65563912606a4cbf92adc02d8

[FIX] fix recording of failed patches with auto-register option Regression from r63441

lib/core/Tiki/Command/UpdateCommand.php
@@ -41,10 +41,10 @@ class UpdateCommand extends Command
 				$output->writeln("<info>Installed: $patch</info>");
 			}
 			foreach (array_keys(\Patch::getPatches([\Patch::NOT_APPLIED])) as $patch) {
-				$output->writeln("<info>Failed: $patch</info>");
+				$output->writeln("<error>Failed: $patch</error>");
 
 				if ($autoRegister) {
-					$installer->recordPatch($patch);
+					\Patch::$list[$patch]->record();
 				}
 			}
 
@@ -63,7 +63,7 @@ class UpdateCommand extends Command
 					$output->writeln("<error>Error $key in $patch\n\t$query\n\t$message</error>");
 
 					if ($autoRegister) {
-						$installer->recordPatch($patch);
+						\Patch::$list[$patch]->record();
 					}
 				}
 			}