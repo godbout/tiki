5cdee9d42c5d9235b13d6f0effb54d080f4ac7f5
chibaguy <chibaguy@users.sourceforge.net>
2017-10-10 08:41:37 +0000

    [UPD] Beginning Less to SCSS transition (update file names, variables, etc.).

--- themes/greenvalley/scss/index.php
+++ themes/greenvalley/scss/index.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id$
+// $Id: index.php 57958 2016-03-17 19:59:37Z jonnybradley $
 
 // This redirects to the sites root to prevent directory browsing
 header("location: ../../../../../tiki-index.php");
