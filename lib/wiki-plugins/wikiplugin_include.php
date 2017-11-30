dd1e0dd80fa3c8f23bbd91e6be31e39bf9412fd4
lfagundes <lfagundes@users.sourceforge.net>
2017-11-30 13:23:27 +0000

    SEFURL in Plugin Include "Read more..." button

--- lib/wiki-plugins/wikiplugin_include.php
+++ lib/wiki-plugins/wikiplugin_include.php
@@ -223,7 +223,8 @@ function wikiplugin_include($dataIn, $params)
 
 	// append a "Read more" link at end of text if only a portion of page is being included
 	if (isset($readmore) && $readmore == 'y') {
-	    $text .= '<p><a href="tiki-index.php?page='.urlencode($page).'" class="btn btn-default">'.smarty_function_icon(['name' => 'align-left'], $smarty).' '.tra("Read more...").'</a><p>';
+        $wikilib = TikiLib::lib('wiki');
+	    $text .= '<p><a href="'.$wikilib->sefurl($page).'" class="btn btn-default">'.smarty_function_icon(['name' => 'align-left'], $smarty).' '.tra("Read more...").'</a><p>';
 	}
 
 	// append an edit button if page_edit_icon does not equal 'n'
