e3132cdaedfc12decb6b6527365fd0a5485aa8c0
chealer <chealer@users.sourceforge.net>
2017-12-07 14:44:08 +0000

    [REF] list_blog_posts(): Clarify/comment

--- lib/blogs/bloglib.php
+++ lib/blogs/bloglib.php
@@ -607,15 +607,17 @@ class BlogLib extends TikiDb_Bridge
 		if (! $allowDrafts) {
 			$mid[] = "`priv`!='y'";
 		} else {
-			// Private posts can be accessed on the following conditions:
-			// user has tiki_p_admin or tiki_p_blog_admin or has written the post
-			// If blog is configured with 'Allow other user to post in this blog', then also if user has tiki_p_blog_post or is owner of this blog
+			/* Private posts can be accessed on the following conditions:
+				user has tiki_p_admin or tiki_p_blog_admin or has written the post
+				If blog is configured with 'Allow other user to post in this blog', then also if user has tiki_p_blog_post or is owner of this blog
+			
+			Basically, if the user can post to the post's blog. */
 			if (! (
 					$tiki_p_blog_admin == 'y'
 					|| (isset($blog_data["public"]) && $blog_data["public"] == 'y' && ($tiki_p_blog_post == 'y' || $ownsblog == 'y'))
 				)
 			) {
-				if (isset($user)) {
+				if (isset($user)) { // Is this needed? How can a user have a post in a blog without owning the blog nor tiki_p_blog_post? Chealer9 2017-12-07
 					$mid[] = "(tbp.`priv`!='y' or tbp.`user`=?)";
 					$bindvars[] = "$user";
 				} else {
