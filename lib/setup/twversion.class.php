d614bd1a66a6e358abe69104f38c1edf9e787781
lfagundes <lfagundes@users.sourceforge.net>
2018-01-29 18:07:16 +0000

    [REL] Preparing 18.0 release

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '18.0svn';	// needs to have no spaces for releases
+		$this->version 	= '18.0';	// needs to have no spaces for releases
 		$this->star	= 'Alcyone';
 		$this->releases	= [];
 
@@ -79,7 +79,7 @@ class TWVersion
 				26 => 'Situla',		// 15.x
 				27 => 'Tabby\'s',		// 16.x
 				28 => 'Zeta Boötis',	// 17.x
-				29 => 'Alcyone',	// 8.x
+				29 => 'Alcyone',	// 18.x
 		];
 	}
 
@@ -260,6 +260,7 @@ class TWVersion
 				'17.1',
 				'18.0alpha',
 				'18.0beta',
+				'18.0',
 			];
 	}
 
