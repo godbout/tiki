9aba01ee866964cb1d56bc306d0c0342f59d14b4
jonnybradley <jonnybradley@users.sourceforge.net>
2018-02-20 18:21:21 +0000

    Add updated combined defunct table removal script in devtools

---
+++ doc/devtools/upto_18_drop_unused_tables.sql
@@ -0,0 +1,25 @@
+# from tiki 4
+DROP TABLE IF EXISTS `galaxia_activities`;
+DROP TABLE IF EXISTS `galaxia_activity_roles`;
+DROP TABLE IF EXISTS `galaxia_instances`;
+DROP TABLE IF EXISTS `galaxia_instance_activities`;
+DROP TABLE IF EXISTS `galaxia_instance_comments`;
+DROP TABLE IF EXISTS `galaxia_processes`;
+DROP TABLE IF EXISTS `galaxia_roles`;
+DROP TABLE IF EXISTS `galaxia_transitions`;
+DROP TABLE IF EXISTS `galaxia_user_roles`;
+DROP TABLE IF EXISTS `galaxia_workitems`;
+DROP TABLE IF EXISTS `tiki_charts`;
+DROP TABLE IF EXISTS `tiki_charts_rankings`;
+DROP TABLE IF EXISTS `tiki_charts_votes`;
+DROP TABLE IF EXISTS `tiki_chart_items`;
+DROP TABLE IF EXISTS `tiki_drawings`;
+DROP TABLE IF EXISTS `tiki_newsreader_marks`;
+DROP TABLE IF EXISTS `tiki_newsreader_servers`;
+DROP TABLE IF EXISTS `tiki_quicktags`;
+# from tiki 7
+DROP TABLE IF EXISTS `tiki_page_drafts`;
+# from tiki 18
+DROP TABLE IF EXISTS `metrics_assigned`;
+DROP TABLE IF EXISTS `metrics_metric`;
+DROP TABLE IF EXISTS `metrics_tab`;
