6c2e6d2d6e0e4d9172b8ac7a1f932ba3cba47ca4
jonnybradley <jonnybradley@users.sourceforge.net>
2018-04-01 08:40:09 +0000

    [MRG] bootstrap4: Part 9 merge of missing changes from trunk...

--- lib/setup/error_reporting.php
+++ lib/setup/error_reporting.php
@@ -28,13 +28,12 @@ if ($prefs['error_reporting_adminonly'] == 'y' and $tiki_p_admin != 'y') {
 
 // Handle Smarty notices
 if (! empty($prefs['smarty_notice_reporting']) and $prefs['smarty_notice_reporting'] === 'y') {
-	$smartyErrorReportingLevel = $errorReportingLevel | E_NOTICE | E_USER_NOTICE ;
-} else {
-	$smartyErrorReportingLevel = $errorReportingLevel;
+	// FIXME: This reports all notices, whether or not they are from Smarty. But if we don't do it, we get no Smarty notices if they are enabled and notices are not. Solving involves clarifying the interface so that $prefs['smarty_notice_reporting'] depends on notices being reported.
+	$errorReportingLevel = $errorReportingLevel | E_NOTICE | E_USER_NOTICE ;
 }
-$smarty->error_reporting = $smartyErrorReportingLevel; // Ensure that Smarty respects the same level of report as Tiki (pref smarty_notice_reporting is already handled above)
+$smarty->error_reporting = $errorReportingLevel; // Ensure that Smarty respects the same level of report as Tiki (pref smarty_notice_reporting is already handled above)
 
-if (php_sapi_name() != 'cli') { // This handler collects errors to display at the bottom of the general template, so don't use it in CLI, otherwise errors would be lost.
+if (php_sapi_name() != 'cli') {
 	set_error_handler('tiki_error_handling', $errorReportingLevel);
 }
 error_reporting($errorReportingLevel);
