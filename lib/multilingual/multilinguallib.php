3b2720f7e75b451130cac0b3fbccf81b2bc3eb57
xorti <xorti@localhost>
2018-06-06 09:12:20 +0000

    [FIX] Fix bidilang setup that required feature_multilang enabled by default

--- lib/multilingual/multilinguallib.php
+++ lib/multilingual/multilinguallib.php
@@ -1230,15 +1230,13 @@ class MultilingualLib extends TikiLib
 	{
 		global $prefs;
 
-		if ($prefs['feature_multilingual'] === 'y') {
-			// Some languages need BiDi support. Add their code names here ...
-			if (Language::isRTL()) {
-				$prefs['feature_bidi'] = 'y';
-				TikiLib::lib('header')->add_cssfile('vendor_bundled/vendor/morteza/bootstrap-rtl/dist/css/bootstrap-rtl.min.css', 99); // 99 is high rank order as it should load after all other css files
-			} else {
-				$prefs['feature_bidi'] = 'n';
-				TikiLib::lib('header')->drop_cssfile('vendor_bundled/vendor/morteza/bootstrap-rtl/dist/css/bootstrap-rtl.min.css');
-			}
+		// Some languages need BiDi support. Add their code names here ...
+		if (Language::isRTL()) {
+			$prefs['feature_bidi'] = 'y';
+			TikiLib::lib('header')->add_cssfile('vendor_bundled/vendor/morteza/bootstrap-rtl/dist/css/bootstrap-rtl.min.css', 99); // 99 is high rank order as it should load after all other css files
+		} else {
+			$prefs['feature_bidi'] = 'n';
+			TikiLib::lib('header')->drop_cssfile('vendor_bundled/vendor/morteza/bootstrap-rtl/dist/css/bootstrap-rtl.min.css');
 		}
 	}
 }
