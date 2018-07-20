13182a22015e0c89b5d7233e8af9d55c9b0a583f
jonnybradley <jonnybradley@users.sourceforge.net>
2018-07-20 18:17:56 +0000

    [FIX] img: Prevent invalid html in image tags

--- lib/wiki-plugins/wikiplugin_img.php
+++ lib/wiki-plugins/wikiplugin_img.php
@@ -619,6 +619,8 @@ function wikiplugin_img($data, $params)
 	$imgdata['sizes']  = '';
 	$imgdata['featured']  = 'n';
 
+	$params = array_map(function ($param) {return  str_replace('"', '%22', $param);}, $params);
+
 	$imgdata = array_merge($imgdata, $params);
 
 	$srcset = '';
@@ -1244,9 +1246,9 @@ function wikiplugin_img($data, $params)
 	} elseif (! empty($imgdata['desc'])) {
 		$replimg .= ' alt="' . $imgdata['desc'] . '"';
 	} elseif (! empty($dbinfo['description'])) {
-		$replimg .= ' alt="' . $dbinfo['description'] . '"';
+		$replimg .= ' alt="' . str_replace('"', '%22', $dbinfo['description']) . '"';
 	} elseif (! empty($dbinfo['name'])) {
-		$replimg .= ' alt="' . $dbinfo['name'] . '"';
+		$replimg .= ' alt="' . str_replace('"', '%22', $dbinfo['name']) . '"';
 	} else {
 		$replimg .= ' alt="Image"';
 	}
@@ -1268,8 +1270,8 @@ function wikiplugin_img($data, $params)
 			$desc = $dbinfo['comment'];
 			$imgname = $dbinfo['comment'];
 		} else {
-			$desc = ! empty($dbinfo['description']) ? $dbinfo['description'] : '';
-			$imgname = ! empty($dbinfo['name']) ? $dbinfo['name'] : '';
+			$desc = ! empty($dbinfo['description']) ? str_replace('"', '%22', $dbinfo['description']) : '';
+			$imgname = ! empty($dbinfo['name']) ? str_replace('"', '%22', $dbinfo['name']) : '';
 		}
 		if (! empty($imgdata['desc'])) {
 			switch ($imgdata['desc']) {
