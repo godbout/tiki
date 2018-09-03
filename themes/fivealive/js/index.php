f27a7d6bda86d7edf137236230de50dbda347913
chibaguy <chibaguy@users.sourceforge.net>
2018-09-03 12:34:28 +0000

    [REF] Remove redundant FiveAliveHS theme; add JS directory to FiveAlive.

--- themes/fivealive/js/index.php
+++ themes/fivealive/js/index.php
@@ -3,8 +3,8 @@
 //
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id$
+// $Id: index.php 66116 2018-04-19 19:04:01Z luciash $
 
 // This redirects to the sites root to prevent directory browsing
-header("location: ../../../../tiki-index.php");
+header("location: ../../../tiki-index.php");
 die;
