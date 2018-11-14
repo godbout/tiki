e3b0c4c804c3b810fb54a1719afe5be848e04fa5
drsassafras <drsassafras@users.sourceforge.net>
2018-11-14 17:31:33 +0000

    [FIX] OCR - uft8mb4 collate

---
+++ installer/schema/20181114_ocr_uff8mb4_tiki.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `tiki_files` CHANGE `ocr_data` `ocr_data` mediumtext COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `tiki_files` CHANGE `ocr_lang` `ocr_lang` mediumtext COLLATE utf8mb4_unicode_ci;
