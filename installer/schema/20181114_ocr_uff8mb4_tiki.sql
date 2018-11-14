09176381f8e6329e5f7a7f04518a9c577ac76fb0
drsassafras <drsassafras@users.sourceforge.net>
2018-11-14 18:19:49 +0000

    [FIX] OCR - ocr_lang should be varchar

--- installer/schema/20181114_ocr_uff8mb4_tiki.sql
+++ installer/schema/20181114_ocr_uff8mb4_tiki.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `tiki_files` CHANGE `ocr_data` `ocr_data` mediumtext COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `tiki_files` CHANGE `ocr_lang` `ocr_lang` mediumtext COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `tiki_files` CHANGE `ocr_lang` `ocr_lang` varchar(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
