5f2ddad9fc460de20e8b3f242ba0b361fe4763a8
lindonb <lindonb@users.sourceforge.net>
2019-02-24 15:07:21 +0000

    module controls: refactor to provide better feedback

--- tiki-module_controls.php
+++ tiki-module_controls.php
@@ -12,7 +12,30 @@ require_once('tiki-setup.php');
 $usermoduleslib = TikiLib::lib('usermodules');
 $smarty = TikiLib::lib('smarty');
 global $tiki_p_configure_modules, $prefs, $user;
-$check_req = (isset($_REQUEST["mc_unassign"]) || isset($_REQUEST["mc_up"]) || isset($_REQUEST["mc_down"]) || isset($_REQUEST["mc_move"]));
+$actions = [
+	'mc_unassign' => [
+		'method' => 'unassign_user_module',
+		'success' => tr('User module unassigned'),
+		'error' => tr('User module not unassigned'),
+	],
+	'mc_up' => [
+		'method' => 'swap_up_user_module',
+		'success' => tr('User module moved up'),
+		'error' => tr('User module not moved up'),
+	],
+	'mc_down' => [
+		'method' => 'swap_down_user_module',
+		'success' => tr('User module moved down'),
+		'error' => tr('User module not moved down'),
+	],
+	'mc_move' => [
+		'method' => 'move_module',
+		'success' => tr('User module moved to opposite side'),
+		'error' => tr('User module not moved to opposite side'),
+	],
+];
+$actions = array_intersect_key($actions, $_REQUEST);
+$check_req = count($actions);
 if ($tiki_p_configure_modules != 'y' && $check_req) {
 	Feedback::errorPage(['mes' => tr('You do not have permission to use this feature'), 'errortype' => 401]);
 }
@@ -23,31 +46,31 @@ if (! $user && $check_req) {
 	Feedback::errorPage(tr('You must log in to use this feature'));
 }
 $request_uri = $url = isset($_SERVER["REQUEST_URI"]) ? $_SERVER['REQUEST_URI'] : '';
-if ($check_req) {
-	//    global $debugger;
-	//    $debugger->msg('Module control clicked: '.$check_req);
-	// Make defaults if user still ot configure modules for himself
-	if (! $usermoduleslib->user_has_assigned_modules($user)) {
-		$usermoduleslib->create_user_assigned_modules($user);
-	}
-	// Handle control icon click
-	if (isset($_REQUEST["mc_up"])) {
-		$usermoduleslib->swap_up_user_module($_REQUEST["mc_up"], $user);
-	} elseif (isset($_REQUEST["mc_down"])) {
-		$usermoduleslib->swap_down_user_module($_REQUEST["mc_down"], $user);
-	} elseif (isset($_REQUEST["mc_move"])) {
-		$usermoduleslib->move_module($_REQUEST["mc_move"], $user);
-	} else {
-		$usermoduleslib->unassign_user_module($_REQUEST["mc_unassign"], $user);
+$access = TikiLib::lib('access');
+foreach ($actions as $action => $settings) {
+	if (isset($_REQUEST[$action]) && $access->checkCsrf()) {
+		// Assign default user modules if user has not yet configured modules
+		if (! $usermoduleslib->user_has_assigned_modules($user)) {
+			$usermoduleslib->create_user_assigned_modules($user);
+		}
+		$method = $settings['method'];
+		$result = $usermoduleslib->$method($_REQUEST[$action], $user);
+		/** @var TikiDb_Pdo_Result|TikiDb_Adodb_Result $result */
+		if ($result && $result->numRows()) {
+			Feedback::success($settings['success']);
+		} else {
+			Feedback::error($settings['error']);
+		}
+
 	}
 	// Remove module movemet paramaters from an URL
 	// \todo What if 'mc_xxx' arg was not at the end? (if smbd fix URL by hands...)
 	//       should I handle this very special (hack?) case?
 	$url = preg_replace('/(.*)(\?|&){1}(mc_up|mc_down|mc_move|mc_unassign)=[^&]*/', '\1', $url);
 }
+
 // Fix locaton if parameter was removed...
 if ($url != $request_uri || (isset($_POST['redirect']) && $_POST['redirect'])) {
-	$access = TikiLib::lib('access');
 	$access->redirect($url);
 }
 $smarty->assign('current_location', $url);
