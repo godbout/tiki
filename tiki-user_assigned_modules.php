7911d80968376d3d303506ddff19ad86cfe513a9
lindonb <lindonb@users.sourceforge.net>
2019-02-24 15:13:05 +0000

    user assigned modules: use method return values to provide feedback on actions

--- tiki-user_assigned_modules.php
+++ tiki-user_assigned_modules.php
@@ -19,7 +19,12 @@ $usermoduleslib = TikiLib::lib('usermodules');
 
 if (isset($_REQUEST["recreate"])) {
 	check_ticket('user-modules');
-	$usermoduleslib->create_user_assigned_modules($user);
+	$result = $usermoduleslib->create_user_assigned_modules($user);
+	if ($result && $result->numRows()) {
+		Feedback::success(tr('Default user modules restored'));
+	} else {
+		Feedback::error(tr('Default user modules not restored'));
+	}
 }
 if (! $usermoduleslib->user_has_assigned_modules($user)) {
 	//	check_ticket('user-modules');
@@ -27,27 +32,57 @@ if (! $usermoduleslib->user_has_assigned_modules($user)) {
 }
 if (isset($_REQUEST["unassign"])) {
 	check_ticket('user-modules');
-	$usermoduleslib->unassign_user_module($_REQUEST["unassign"], $user);
+	$result = $usermoduleslib->unassign_user_module($_REQUEST["unassign"], $user);
+	if ($result && $result->numRows()) {
+		Feedback::success(tr('User module unassigned'));
+	} else {
+		Feedback::error(tr('User module not unassigned'));
+	}
 }
 if (isset($_REQUEST["assign"])) {
 	check_ticket('user-modules');
-	$usermoduleslib->assign_user_module($_REQUEST["module"], $_REQUEST["position"], $_REQUEST["order"], $user);
+	$result = $usermoduleslib->assign_user_module($_REQUEST["module"], $_REQUEST["position"], $_REQUEST["order"], $user);
+	if ($result && $result->numRows()) {
+		Feedback::success(tr('User module assigned'));
+	} else {
+		Feedback::error(tr('User module not assigned'));
+	}
 }
 if (isset($_REQUEST["up"])) {
 	check_ticket('user-modules');
-	$usermoduleslib->up_user_module($_REQUEST["up"], $user);
+	$result = $usermoduleslib->up_user_module($_REQUEST["up"], $user);
+	if ($result && $result->numRows()) {
+		Feedback::success(tr('User module display order moved up. Displayed order may not change if other modules now have the same order rank.'));
+	} else {
+		Feedback::error(tr('User module display order not moved up'));
+	}
 }
 if (isset($_REQUEST["down"])) {
 	check_ticket('user-modules');
-	$usermoduleslib->down_user_module($_REQUEST["down"], $user);
+	$result = $usermoduleslib->down_user_module($_REQUEST["down"], $user);
+	if ($result && $result->numRows()) {
+		Feedback::success(tr('User module display order moved down. Displayed order may not change if other modules now have the same order rank.'));
+	} else {
+		Feedback::error(tr('User module display order not moved down'));
+	}
 }
 if (isset($_REQUEST["left"])) {
 	check_ticket('user-modules');
-	$usermoduleslib->set_column_user_module($_REQUEST["left"], $user, 'left');
+	$result = $usermoduleslib->set_column_user_module($_REQUEST["left"], $user, 'left');
+	if ($result && $result->numRows()) {
+		Feedback::success(tr('User module moved to left column'));
+	} else {
+		Feedback::error(tr('User module not moved to left column'));
+	}
 }
 if (isset($_REQUEST["right"])) {
 	check_ticket('user-modules');
-	$usermoduleslib->set_column_user_module($_REQUEST["right"], $user, 'right');
+	$result = $usermoduleslib->set_column_user_module($_REQUEST["right"], $user, 'right');
+	if ($result && $result->numRows()) {
+		Feedback::success(tr('User module moved to right column'));
+	} else {
+		Feedback::error(tr('User module not moved to right column'));
+	}
 }
 $orders = [];
 for ($i = 1; $i < 50; $i++) {
