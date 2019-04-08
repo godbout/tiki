d929eba2a01f691f54b7d15c833b95f7bc6d8e69
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2019-04-08 02:29:59 -0300

    [NEW] Create a module do display information about local git repository

---
+++ modules/mod-func-git_detail.php
@@ -0,0 +1,49 @@
+<?php
+// (c) Copyright by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+//this script may only be included - so its better to die if called directly.
+if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
+	header("location: index.php");
+	exit;
+}
+
+/**
+ * Return module information
+ *
+ * @return array
+ */
+function module_git_detail_info()
+{
+	return [
+		'name' => tra('GIT detail'),
+		'description' => tra('GIT commit and last update information.'),
+		'params' => [],
+	];
+}
+
+/**
+ * Collect information about current git repository and assign information
+ * on smarty template engine
+ *
+ * @param $mod_reference
+ * @param $module_params
+ */
+function module_git_detail($mod_reference, $module_params)
+{
+	$smarty = TikiLib::lib('smarty');
+	$gitlib = TikiLib::lib('git');
+	$content = [];
+
+	try {
+		$content = $gitlib->get_info();
+	} catch (Exception $e) {
+		$error = strval($e);
+	}
+
+	$smarty->assign('error', $error);
+	$smarty->assign('content', $content);
+}
---
+++ templates/modules/mod-git_detail.tpl
@@ -0,0 +1,22 @@
+{tikimodule
+    decorations="{$module_params.decorations}"
+    error="{$error}" 
+    flip="{$module_params.flip}"
+    nobox="{$module_params.nobox}"
+    nonums="{$module_params.nonums}"
+    notitle="{$module_params.notitle}"
+    overflow="{$module_params.overflow}"
+    title="{$module_params.title}"
+    style="{$module_params.style}"
+    }
+<div class="mod-git_detail">
+    <p>
+        <span class="label">{tr}Last updated{/tr}</span>
+        <span class="branch">(GIT {$content.branch}:{$content.commit.hash|substring:0:5}):</span>
+        <span class="date">{$content.author.date|tiki_long_datetime}</span>
+    </p>
+    {if $error}
+        <p class="error">{$error}</p>
+    {/if}
+</div>
+{/tikimodule}
