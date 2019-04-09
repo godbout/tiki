28dbea8e2a52e8b350d518bcef7fddb494df03fc
jonnybradley <jonnybradley@users.sourceforge.net>
2019-04-09 16:28:18 +0000

    [FIX] console: Add a wrapper for doRunCommand so we can run Feedback::printToConsole afterwards to expose all tiki errors which are usually hidden (apart from index:rebuild and a couple of others that call this function specifically already)

--- lib/core/Tiki/Command/Application.php
+++ lib/core/Tiki/Command/Application.php
@@ -3,14 +3,17 @@
 namespace Tiki\Command;
 
 use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends SymfonyApplication
 {
 	/**
 	 * Gets the default input definition.
 	 *
-	 * @return InputDefinition An InputDefinition instance
+	 * @return \Symfony\Component\Console\Input\InputDefinition An InputDefinition instance
 	 */
 	protected function getDefaultInputDefinition()
 	{
@@ -19,5 +22,26 @@ class Application extends SymfonyApplication
 		$definition->addOption(new InputOption('--as-user', '', InputOption::VALUE_REQUIRED, 'Run the command as a different user'));
 
 		return $definition;
+	}
+
+	/**
+	 * Runs the current command.
+	 *
+	 * Calls the parent method then prints all Tiki Feedback (errors and messages) to the console
+	 *
+	 * @param Command         $command
+	 * @param InputInterface  $input
+	 * @param OutputInterface $output
+	 *
+	 * @return int 0 if everything went fine, or an error code
+	 * @throws \Throwable
+	 */
+	protected function doRunCommand(Command $command, InputInterface $input, OutputInterface $output)
+	{
+		$exitCode = parent::doRunCommand($command, $input, $output);
+
+		\Feedback::printToConsole($output);
+
+		return $exitCode;
 	}
 }
