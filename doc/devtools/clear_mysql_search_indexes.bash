ea60e9658dcb88b9e001650eb7128e7f3d9285ef
Jyhem <jyhem@users.sourceforge.net>
2019-04-19 22:52:24 +0000

    [ENH] Only delete the unused indexes

--- doc/devtools/clear_mysql_search_indexes.bash
+++ doc/devtools/clear_mysql_search_indexes.bash
@@ -5,21 +5,36 @@
 # Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 #
 # Clean mysql indexes because they tend to pile up and only one is actually being used
-# TODO: exclude indexes index_pref_*
-# TODO: do not delete current index as seen in "unified_mysql_index_current"(tiki_preferences)
-# TODO: check that full-text search is configured before suggesting php console.php index:rebuild (`tiki_preferences` VALUES ('unified_engine','mysql');)
+# TODO: non-interactive silent mode for crontab
+# TODO: allow deletion of current index
 
 usage()
 {
-	echo "Usage: $0 <path/to/local.php>"
+	echo "Usage: $0 [path/to/local.php]"
 }
 
-echo "== $1"
-if [[ -f "$1" ]]
+if [[ "$1" = "" ]]
 then
-  DB_DETECTED=$(grep "^\$dbs_tiki" "$1" | cut -d"'" -f2| tail -n 1);
-  USER_DETECTED=$(grep "^\$user_tiki" "$1" | cut -d"'" -f2| tail -n 1);
-  PASS_DETECTED=$(grep "^\$pass_tiki" "$1" | cut -d"'" -f2| tail -n 1);
+	# If no parameter was given, try to guess
+	if [[ -f db/local.php ]]
+	then
+		LOCALFILE="db/local.php"
+		echo "Detected: $LOCALFILE"
+	else
+		usage
+		exit 1
+	fi
+else
+	# Use the one provided
+	LOCALFILE="$1"
+fi
+
+echo "Using: $LOCALFILE"
+if [[ -f "$LOCALFILE" ]]
+then
+  DB_DETECTED=$(grep "^\$dbs_tiki" "$LOCALFILE" | cut -d"'" -f2| tail -n 1);
+  USER_DETECTED=$(grep "^\$user_tiki" "$LOCALFILE" | cut -d"'" -f2| tail -n 1);
+  PASS_DETECTED=$(grep "^\$pass_tiki" "$LOCALFILE" | cut -d"'" -f2| tail -n 1);
 else
   echo "ERROR: local.php file not found"
 	usage
@@ -28,17 +43,38 @@ fi
 
 INDEXES=$(mysql --skip-column-names --silent -u "${USER_DETECTED}" --password="${PASS_DETECTED}" "${DB_DETECTED}" -e "SHOW TABLES WHERE Tables_in_${DB_DETECTED} LIKE 'index_%';")
 NB_INDEXES=$(mysql --skip-column-names --silent -u "${USER_DETECTED}" --password="${PASS_DETECTED}" "${DB_DETECTED}" -e "SHOW TABLES WHERE Tables_in_${DB_DETECTED} LIKE 'index_%';"| wc -l)
+SEARCHENGINE=$(mysql --skip-column-names --silent -u "${USER_DETECTED}" --password="${PASS_DETECTED}" "${DB_DETECTED}" -e "SELECT value FROM tiki_preferences WHERE name = 'unified_engine';" )
+if [[ "SEARCHENGINE" = "mysql" ]]
+then
+	CURRENTINDEX=$(mysql --skip-column-names --silent -u "${USER_DETECTED}" --password="${PASS_DETECTED}" "${DB_DETECTED}" -e "SELECT value FROM tiki_preferences WHERE name = 'unified_mysql_index_current';" )
+	echo "Current index: $CURRENTINDEX"
+fi
+echo "Search engine: $SEARCHENGINE"
 echo "---"
 echo "$INDEXES"
+if [[ "$NB_INDEXES" = "0" ]]
+then
+	echo "No index found. Nothing to do."
+	exit 0
+fi
 echo "${NB_INDEXES} indexes found. Delete them? (y/N)"
 read delete
 if [ "$delete" = "y" ]
 then
 	for i in $INDEXES
 	do
-		echo "deleting $i"
-		mysql -u "${USER_DETECTED}" --password="${PASS_DETECTED}" "${DB_DETECTED}" -e "DROP TABLE IF EXISTS `$i`;"
+		if [ "$i" != "$CURRENTINDEX" ]
+		then
+			mysql -u "${USER_DETECTED}" --password="${PASS_DETECTED}" "${DB_DETECTED}" -e "DROP TABLE IF EXISTS \`$i\`;"
+			echo "deleting $i"
+		else
+			echo "keeping $i"
+		fi
 	done
-	echo "Now may be the perfect time for: php console.php index:rebuild"
+	# Check that full-text search is configured before suggesting php console.php index:rebuild
+	if [ "$SEARCHENGINE" = "mysql" ]
+	then
+		echo "Now may be a good time for: php console.php index:rebuild"
+	fi
 fi
 
