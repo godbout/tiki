d7861d8122d03e246c09603f15381a922d7dac86
mnsuccess <ngudiamanasse@gmail.com>
2019-04-28 20:41:14 +0200

    [ENH] Page for when a Tiki site is closed
    (refs https://gitlab.com/tikiwiki/tiki/merge_requests/106)

--- tiki-error_simple.php
+++ tiki-error_simple.php
@@ -11,26 +11,34 @@ $filter = \TikiFilter::get('xss');
 if (! empty($_REQUEST['error'])) {
 	$error = $filter->filter(substr($_REQUEST["error"], 0, 256));
 } else {
-	$error = 'There was an unspecified error.  Please go back and try again.';
+	$error = '';
 }
 if (! empty($_REQUEST['title'])) {
 	$title = $filter->filter($_REQUEST['title']);
 } else {
-	$title = 'Maintenance';
+	$title = '';
 }
 
 session_start();
 $ticket = strtr(str_replace('=', '', base64_encode(\phpseclib\Crypt\Random::string(32))), '+/', '-_');
 $_SESSION['tickets'][$ticket] = time();
 
-$login = '<form name="loginbox" action="tiki-login.php?page=tikiIndex" method="post"><table><tr><td>' .
-	'User:</td><td><input type="text" name="user"  size="20" /></td></tr><tr><td>' .
-	'Pass:</td><td><input type="password" name="pass" size="20" /></td></tr><tr><td style="text-align: center;" colspan="2">'.
-	'<input type="hidden" class="ticket" name="ticket" value="' . $ticket . '" /><input type="hidden" name="confirmForm" value="y" />'.
-	'<input type="submit" name="login" value="login" class="btn btn-default" /></td></tr></table></form>';
-
-$back = '<p><a href="javascript:history.back()">Go back</a></p>';
-
+$login = '<form class="form-detail" id="myform" action="tiki-login.php?page=tikiIndex" method="post">
+			<div class="form-row">
+				<label>User</label>
+				<input type="text" name="user" id="your_email" class="input-text" required >
+			</div>
+			<div class="form-row form-row-1 ">
+				<label for="password">Password</label>
+				<input type="password" name="pass" id="password" class="input-text" required>
+			</div>
+			<div class="form-row-last">
+				<input type="hidden" class="ticket" name="ticket" value="' . $ticket . '" />
+				<input type="hidden" name="confirmForm" value="y" />
+				<input type="submit" name="login" value="login" class="register" value="Register">
+			</div>
+			</form>';
+			
 if (file_exists('themes/base_files/other/site_closed_local.html')) {
 	$html = file_get_contents('themes/base_files/other/site_closed_local.html');
 } else {
@@ -40,8 +48,6 @@ if (file_exists('themes/base_files/other/site_closed_local.html')) {
 $html = str_replace('{error}', $error, $html);
 $html = str_replace('{title}', $title, $html);
 $html = str_replace('{login}', $login, $html);
-$html = str_replace('{back}', $back, $html);
-
 header("HTTP/1.0 503 Service Unavailable");
 
 echo $html;
