godbout/tiki

commit e240ea16fbbdaa1f1fe44829c6bb4b8c70200bb0

[ADD] allow setting rating options like "1=label1,2=label2,3=label3", as radio button options for forum posts
git-svn-id: file:///var/local/code.tiki.org/tikisvn/trunk@51045 b456876b-0849-0410-b77d-98878d47e9d5

lib/smarty_tiki/function.preference.php
@@ -40,7 +40,7 @@ function smarty_function_preference( $params, $smarty )
 			if (count($info['pages']) > 0) {
 			foreach ($info['pages'] as $pg) {
 				$ct_string = $pg[1] > 1 ? '&amp;cookietab=' . $pg[1] : '';
-				$pages_string .= '<a class="lm_result label label-default" href="tiki-admin.php?page=' . $pg[0] . $ct_string . '&amp;highlight=' . $info['preference'] . '">' . $pg[0] . '</a> ';
+				$pages_string = '<a class="lm_result label label-default" href="tiki-admin.php?page=' . $pg[0] . $ct_string . '&amp;highlight=' . $info['preference'] . '">' . $pg[0] . '</a> ';
 			}
 			} else {
 				$pages_string = tra('(not found in an admin panel)');
@@ -74,7 +74,11 @@ function smarty_function_preference( $params, $smarty )
 			$smarty->assign('syntax', $params['syntax']);
 		}
 
-		return $smarty->fetch('prefs/' . $info['type'] . '.tpl', $params['name']);
+        if (file_exists('templates/prefs/' . $info['type'] . '.tpl')) {
+		    return $smarty->fetch('prefs/' . $info['type'] . '.tpl', $params['name']);
+        } else {
+            return $smarty->fetch('prefs/text.tpl');
+        }
 	} else {
 		$info = array(
 			'value' => tra('Error'),

lib/prefs/wiki.php
@@ -292,9 +292,7 @@ function prefs_wiki_list($partial = false)
 			'name' => tra('Wiki rating options'),
 			'description' => tra('List of options available for the rating of wiki comments.'),
 			'type' => 'text',
-			'separator' => ',',
-			'filter' => 'int',
-			'default' => range(1, 5),
+			'default' => "1,2,3,4,5",
 		),
 		'wiki_uses_slides' => array(
 			'name' => tra('Add a slideshow button on wiki pages'),

lib/rating/ratinglib.php
@@ -140,8 +140,7 @@ class RatingLib extends TikiDb_Bridge
 
 	function record_user_vote( $user, $type, $objectId, $score, $time = null )
 	{
-		global $prefs;
-		$tikilib = TikiLib::lib('tiki');
+		global $tikilib, $prefs;
 		if ( ! $this->is_valid($type, $score, $objectId) ) {
             return false;
 		}
@@ -180,15 +179,19 @@ class RatingLib extends TikiDb_Bridge
 
 	function is_valid( $type, $value, $objectId )
 	{
-		$options = $this->get_options($type, $objectId);
+		$options = $this->get_options($type, $objectId, false, $hasLabel);
 
-		return in_array($value, $options);
+        if($hasLabel){
+            return array_key_exists($value, $options);
+        }
+		    return in_array($value, $options);
+
 	}
 
-	function get_options( $type, $objectId, $skipOverride = false )
+	function get_options( $type, $objectId, $skipOverride = false, &$hasLabels = false )
 	{
 		$pref = 'rating_default_options';
-
+        $expectedArray = true;
 		switch( $type ) {
 			case 'wiki page':
 				$pref = 'wiki_simple_ratings_options';
@@ -201,10 +204,12 @@ class RatingLib extends TikiDb_Bridge
 				break;
 			case 'forum':
 				$pref = 'wiki_comments_simple_ratings_options';
+                $expectedArray = false;
 				break;
 		}
 
-		$tikilib = TikiLib::lib('tiki');
+		global $tikilib,
+               $prefs;
 
 		$override = $this->get_override($type, $objectId);
 
@@ -212,11 +217,21 @@ class RatingLib extends TikiDb_Bridge
 			return $override;
 		}
 
-		return $tikilib->get_preference($pref, range(1, 5), true);
+        $value = $prefs[$pref];
+
+        if ( is_string($value) && strpos($value, '=') !== false ){
+            $hasLabels = true;
+            $parser = new WikiLingo\Utilities\Parameters\Parser();
+            $parsedPref = $parser->parse($value);
+            return $parsedPref;
+        }
+
+		return $tikilib->get_preference($pref, range(1, 5), $expectedArray);
 	}
 
 	function set_override($type, $objectId, $value)
 	{
+		global $attributelib;
 		$options = $this->override_array($type);
 
 		$attributelib->set_attribute($type, $objectId, $type.".rating.override", $options[$value - 1]);
@@ -310,14 +325,23 @@ class RatingLib extends TikiDb_Bridge
 			$votings[$user_voting['optionId']]++;
 		}
 
-		$voteOptionsOverride = $this->get_options($type, $threadId);
+		$voteOptionsOverride = $this->get_options($type, $threadId, false, $hasLabels);
 		ksort($voteOptionsOverride);
 		$voteOptionsGeneral = $this->get_options($type, $threadId, true);
 		ksort($voteOptionsGeneral);
 
-
-		$overrideMin = (int)$voteOptionsOverride[0];
-		$overrideMax = (int)$voteOptionsOverride[count($voteOptionsOverride) - 1];
+        if ($hasLabels){
+            ksort($voteOptionsOverride);
+            $overrideMin = key($voteOptionsOverride);
+            end($voteOptionsOverride);
+            $overrideMax = key($voteOptionsOverride);
+        }
+        else
+        {
+            ksort($voteOptionsOverride);
+            $overrideMin = reset($voteOptionsOverride);
+            $overrideMax = end($voteOptionsOverride);
+        }
 
 		//$generalMin = (int)$voteOptionsGeneral[0];
 		//$generalMax = (int)$voteOptionsGeneral[count($voteOptionsGeneral) - 1];
@@ -468,7 +492,7 @@ class RatingLib extends TikiDb_Bridge
 
 	function get_options_smiles($type, $objectId = 0, $sort = false)
 	{
-		$options = $this->get_options($type, $objectId);
+		$options = $this->get_options($type, $objectId, false, $hasLabels);
 		$colors = $this->get_options_smiles_colors();
 
 		$optionsAsKeysSorted = array();
@@ -516,7 +540,7 @@ class RatingLib extends TikiDb_Bridge
 
 	private function internal_refresh_rating( $type, $object, $runner, $configurations )
 	{
-		$ratingconfiglib = TikiLib::lib('ratingconfig');
+		global $ratingconfiglib; require_once 'lib/rating/configlib.php';
 		$runner->setVariables(array('type' => $type, 'object-id' => $object));
 
 		foreach ( $configurations as $config ) {
@@ -561,4 +585,6 @@ class RatingLib extends TikiDb_Bridge
 		return $this->configurations = $configurations;
 	}
 }
+
+global $ratinglib; $ratinglib = new RatingLib;
 

lib/smarty_tiki/function.rating.php
@@ -60,7 +60,7 @@ function smarty_function_rating( $params, $smarty )
 	}
 
 	$vote = $ratinglib->get_vote($type, $id);
-	$options = $ratinglib->get_options($type, $id);
+	$options = $ratinglib->get_options($type, $id, false, $hasLabels);
 
 	if ($prefs['rating_smileys'] == 'y') {
 		$smiles = $ratinglib->get_options_smiles($type, $id);
@@ -71,6 +71,7 @@ function smarty_function_rating( $params, $smarty )
 	$smarty->assign('rating_id', $id);
 	$smarty->assign('rating_options', $options);
 	$smarty->assign('current_rating', $vote);
+	$smarty->assign('rating_has_labels', $hasLabels);
 	return $smarty->fetch('rating.tpl');
 }
 

lib/smarty_tiki/function.rating_result.php
@@ -10,7 +10,7 @@ function smarty_function_rating_result( $params, $smarty )
 	global $prefs;
 	$ratinglib = TikiLib::lib('rating');
 	$votings = $ratinglib->votings($params['id'], $params['type']);
-	$smiles = $ratinglib->get_options_smiles($params['type'], $params['id'], true);
+	$smiles = ($prefs['rating_smileys'] == 'y' ? $ratinglib->get_options_smiles($params['type'], $params['id'], true) : null);
 	$tableBody = "";
 
 	if ($prefs['rating_results_detailed'] == 'y') {

templates/rating.tpl
@@ -6,6 +6,11 @@
 					<img src="{$rating_smiles[$i].img|escape}" title="{tr}User rating:{/tr} {$v|escape}">
 			</label>
 		{/foreach}
+    {elseif $rating_has_labels}
+        {foreach from=$rating_options item=v key=i}
+            <input type="radio" name="rating_value[{$rating_type|escape}][{$rating_id|escape}]" value="{$i|escape}" id="{$rating_type|escape}{$rating_id|escape}_{$v|escape}" {if $current_rating == $v}checked="checked"{/if}>
+            <label for="{$rating_type|escape}{$rating_id|escape}_{$v|escape}">{$v|escape}</label>
+        {/foreach}
 	{else}
 		{foreach from=$rating_options item=v key=i}
 			<input type="radio" name="rating_value[{$rating_type|escape}][{$rating_id|escape}]" value="{$v|escape}" id="{$rating_type|escape}{$rating_id|escape}_{$v|escape}" {if $current_rating == $v}checked="checked"{/if}>

commit f102bef138516a0396b6e77f2d473bfc126e3595

[FIX] prefs: Restore showing of multiple control panel locations for prefs searches and format it a bit better (Wow, has this really been broken since r51045? nothing to do with "[ADD] allow setting rating options like "1=label1,2=label2,3=label3", as radio button options for forum posts" anyway!)

lib/smarty_tiki/function.preference.php
@@ -41,17 +41,17 @@ function smarty_function_preference($params, $smarty)
 			$info['tagstring'] .= ' modified';
 		}
 
+		$pages_string = '';
 		if ($get_pages) {
 			if (count($info['pages']) > 0) {
 				foreach ($info['pages'] as $pg) {
 					$ct_string = $pg[1] > 1 ? '&amp;cookietab=' . $pg[1] : '';
-					$pages_string = '<a class="lm_result label label-default" href="tiki-admin.php?page=' . $pg[0] . $ct_string . '&amp;highlight=' . $info['preference'] . '">' . $pg[0] . '</a> ';
+					$pages_string .= ($pages_string ? ', ' : '');
+					$pages_string .= '<a class="lm_result label label-default" href="tiki-admin.php?page=' . $pg[0] . $ct_string . '&amp;highlight=' . $info['preference'] . '">' . $pg[0] . '</a>';
 				}
 			} else {
 				$pages_string = tra('(not found in an admin panel)');
 			}
-		} else {
-			$pages_string = '';
 		}
 		$info['pages'] = $pages_string;
 