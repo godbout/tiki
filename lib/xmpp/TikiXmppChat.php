9f8c4715a72d08166f160c2548a5d95b48e35a9b
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2019-05-15 16:49:51 +0530

    [ENH] Create XMPP room by using XMPP protocol only

--- lib/xmpp/TikiXmppChat.php
+++ lib/xmpp/TikiXmppChat.php
@@ -11,23 +11,23 @@ use Fabiang\Xmpp\Protocol\Roster;
 use Fabiang\Xmpp\Protocol\Presence;
 use Fabiang\Xmpp\Protocol\Invitation;
 use Fabiang\Xmpp\Protocol\Message;
+use Fabiang\Xmpp\Protocol\GroupChatCreate;
 use Fabiang\Xmpp\Protocol\ProtocolImplementationInterface;
 use Fabiang\Xmpp\Util\XML;
-
 
 class TikiXmppChat
 {
 	public $client;
 
-	public function __construct($params=array())
+	public function __construct($params = [])
 	{
-		$default = array(
+		$default = [
 			"scheme" => "tcp",
 			"host" => "",
 			"port" => 5222,
 			"user" => "",
 			"pass" => "",
-		);
+		];
 		$params = array_merge($default, $params);
 		$address = "{$params['scheme']}://{$params['host']}:{$params['port']}";
 
@@ -35,12 +35,20 @@ class TikiXmppChat
 		$options->setUsername($params['user']);
 		$options->setPassword($params['pass']);
 
+		$options->setContextOptions(
+			[
+				'ssl' => ['verify_peer' => false]
+			]
+		);
+
 		$this->setClient(new Client($options));
 	}
 
 	public function connect()
 	{
-		$this->getClient()->getConnection()->connect();
+		$conn = $this->getConnection();
+		$conn->connect();
+		$conn->getSocket()->setBlocking(false);
 		return $this;
 	}
 
@@ -53,12 +61,17 @@ class TikiXmppChat
 	public function getClient()
 	{
 		return $this->client;
-	} 	
+	}
 
 	public function setClient($client)
 	{
 		$this->client = $client;
 		return $this;
+	}
+
+	public function getConnection()
+	{
+		return $this->getClient()->getConnection();
 	}
 
 	public function getJid()
@@ -84,7 +97,7 @@ class TikiXmppChat
 		return $this;
 	}
 
-	public function sendMessage($message, $to, $type=Message::TYPE_CHAT)
+	public function sendMessage($message, $to, $type = Message::TYPE_CHAT)
 	{
 		$this->getClient()->send(new Message($message, $to, $type));
 		return $this;
@@ -94,5 +107,39 @@ class TikiXmppChat
 	{
 		$this->getClient()->send(new Presence($priority, $to, $nickname));
 		return $this;
+	}
+
+	public function createRoom($owner, $room, $attr = [])
+	{
+		$conn = $this->getConnection();
+		$this->getClient()->send(new GroupChatCreate($owner, $room));
+
+		$attempts = 5;
+		$dom = null;
+		while ($attempts > 0 || $dom) {
+			$dom = $conn->receive();
+
+			if ($dom) {
+				$xpath = new DOMXpath($dom);
+				$x = $xpath->query('//x');
+				$x = $x->length > 0 ? $x[0] : null;
+				$test = ! is_null($x);
+				$test = $test && $x->parentNode->nodeName === 'presence';
+				$test = $test && $x->parentNode->getAttribute('from') === $room;
+				$test = $test && $x->parentNode->getAttribute('to') === $owner;
+
+				// probably our response
+				if ($test) {
+					$test = $xpath->query('status[@code=201]', $x)->length > 0
+						&& $xpath->query('status[@code=110]', $x)->length > 0;
+					return $test;
+				}
+			} else {
+				$attempts -= 1;
+				sleep(1);
+			}
+		}
+
+		return false;
 	}
 }
