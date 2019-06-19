134d2f2d058fa58fccdc99161f8e8cdf9ba5cc35
robertokir <robertokir@users.sourceforge.net>
2019-06-19 15:15:10 +0000

    [REL] Preparing 20.0RC1 release

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '20.0svn';	// needs to have no spaces for releases
+		$this->version 	= '20.0RC1';	// needs to have no spaces for releases
 		$this->star		= 'Tarazed';
 		$this->releases	= [];
 
@@ -277,6 +277,7 @@ class TWVersion
 				'19.1',
 				'20.0alpha',
 				'20.0beta',
+				'20.0RC1',
 			];
 	}
 
