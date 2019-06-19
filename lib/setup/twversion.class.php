2c55ad2de6245470f616fdd3b1d8adca8da6213f
robertokir <robertokir@users.sourceforge.net>
2019-06-19 17:43:21 +0000

    [REL] Closing 20.0RC1 release

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '20.0RC1';	// needs to have no spaces for releases
+		$this->version 	= '20.0svn';	// needs to have no spaces for releases
 		$this->star		= 'Tarazed';
 		$this->releases	= [];
 
