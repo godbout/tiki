289f76b25822af59642c1552ea8d8bc38aed8ef2
drsassafras <drsassafras@users.sourceforge.net>
2019-07-04 18:49:57 +0000

    [ENH] OCR - Add CLI options to filter changes based on current OCR status.

--- lib/core/Tiki/Command/OCRSetCommand.php
+++ lib/core/Tiki/Command/OCRSetCommand.php
@@ -11,6 +11,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use TikiFilter;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
@@ -27,8 +28,33 @@ class OCRSetCommand extends Command
 				'Do you want to Queue the files for OCR processing, or mark the files to be skipped. Valid options = (q queue s skip).')
 			->addArgument(
 				'File ID',
-				InputArgument::REQUIRED,
-				'Specify a file id, or a range to mark. eg. (12 or 90:92)');
+				InputArgument::OPTIONAL,
+				'Specify a file id, or a range to filter or omit to match all. eg. (12 or 90:92)')
+			->addOption(
+				'stalled',
+				's',
+				InputOption::VALUE_NONE,
+				'Filter files so only stalled files are updated')
+			->addOption(
+				'finished',
+				'f',
+				InputOption::VALUE_NONE,
+				'Filter only files that are already finished to be updated')
+			->addOption(
+				'processing',
+				'p',
+				InputOption::VALUE_NONE,
+				'Filter only files that are currently being processed to be updated')
+			->addOption(
+				'queued',
+				'u',
+				InputOption::VALUE_NONE,
+				'Filter only files that are queued for OCR to be updated.')
+			->addOption(
+				'refrained',
+				'r',
+				InputOption::VALUE_NONE,
+				'Filter only files not marked for OCR processing to be updated');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
@@ -36,39 +62,57 @@ class OCRSetCommand extends Command
 		$ocrLib = \TikiLib::lib('ocr');
 		$outputStyle = new OutputFormatterStyle('red');
 		$output->getFormatter()->setStyle('error', $outputStyle);
+
+		$optionCount = array_sum([$input->getOption('stalled'),
+								  $input->getOption('finished'),
+								  $input->getOption('processing'),
+								  $input->getOptions('queued'),
+								  $input->getOptions('refrained')]);
+		$conditions = [];
+		$update = $ocrLib->table('tiki_files');
+		if ($optionCount > 1){
+			$output->writeln('<error>You may only specify 1 option</error>');
+			return;
+		}elseif ($optionCount === 1) {
+			if ($input->getOption('stalled')) {
+				$conditions['ocr_state'] = $ocrLib::OCR_STATUS_STALLED;
+			} elseif ($input->getOption('finished')) {
+				$conditions['ocr_state'] = $ocrLib::OCR_STATUS_FINISHED;
+			} elseif ($input->getOption('processing')) {
+				$conditions['ocr_state'] = $ocrLib::OCR_STATUS_PROCESSING;
+			} elseif ($input->getOption('queued')) {
+				$conditions['ocr_state'] = $ocrLib::OCR_STATUS_PENDING;
+			} elseif ($input->getOption('refrained')) {
+				$conditions['ocr_state'] = $ocrLib::OCR_STATUS_SKIP;
+			}
+		}
+
 		$task = $input->getArgument('Queue or Skip');
 		$task = strtolower($task);
 
 		if ($task[0] === 'q'){
-			$state = ['ocr_state' => $ocrLib::OCR_STATUS_PENDING];
+			$state['ocr_state'] = $ocrLib::OCR_STATUS_PENDING;
 		}elseif ($task[0] === 's'){
-			$state = ['ocr_state' => $ocrLib::OCR_STATUS_SKIP];
+			$state['ocr_state'] = $ocrLib::OCR_STATUS_SKIP;
 		}else{
 			$output->writeln('<error>Must specify a valid option. Use Q to Queue files or S to Skip Files.</error>');
 			return;
 		}
-
 		$range = TikiFilter::get('digitscolons')->filter($input->getArgument('File ID'));
 		$range = explode(':',$range);
-		$optionsCount = count($range);
-		$update = $ocrLib->table('tiki_files');
+		$range = asort($range);											// we need lower values first for search results to match
 
-		if ($optionsCount === 1) {
-			$updated = $update->update(
-				$state,
-				['fileId' => $range[0]]);
-			if ($updated->numrows()) {
-				$output->writeln("<comment>File status updated</comment>");
-			}else{
-				$output->writeln("<error>File could not be updated</error>");
-			}
-		} elseif (($optionsCount === 2)){
-			$updated = $update->updateMultiple(
-				$state,
-				['fileId' => $update->between($range)]);
-			$output->writeln('<comment>Status of ' . $updated->numrows() . ' files updated.</comment>');
-		} else {
-			$output->writeln('<error>Must specify a valid File ID or File ID Range eg. (12 or 90:92)</error>');
+		if (!empty($range[1])) {
+			$conditions['fileId'] = $update->between($range);
+		}elseif(!empty($range[0])){
+			$conditions['fileId'] = $range[0];
 		}
+
+		$updated = $update->updateMultiple($state,$conditions);
+		$numrows = $updated->numrows;
+		if (!$updated->numrows){
+			$numrows = '<error>' . $numrows . '</error>';
+		}
+		$output->writeln('<comment>Status of ' . $numrows . ' files updated.</comment>');
 	}
 }
