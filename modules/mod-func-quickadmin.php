f155cc1dc116dbcde82d67ea66e1411f8bcc95f8
Jyhem <jyhem@users.sourceforge.net>
2019-08-19 14:48:19 +0000

    [UX] Give a choice on what is shown

--- modules/mod-func-quickadmin.php
+++ modules/mod-func-quickadmin.php
@@ -25,6 +25,18 @@ function module_quickadmin_info()
 				'name' => tra('Mode'),
 				'description' => tra('Display mode: module or header. Leave empty for module mode'),
 			],
+			'display' => [
+				'name' => tra('Display'),
+				'description' => tra('Shown: Display shortcuts or preferences history or both (both / shortcuts / history)'),
+				'filter' => 'alpha',
+				'default' => 'both',
+				'since' => '21',
+				'options' => [
+					['text' => tra('Both'), 'value' => 'both'],
+					['text' => tra('Only shortcuts'), 'value' => 'shortcuts'],
+					['text' => tra('Only preferences history'), 'value' => 'history'],
+				],
+			],
 		]
 	];
 }
@@ -36,5 +48,14 @@ function module_quickadmin_info()
 function module_quickadmin($mod_reference, $module_params)
 {
 
-	TikiLib::lib('smarty')->assign('recent_prefs', TikiLib::lib('prefs')->getRecent());
+	if ($module_params['display'] == 'shortcuts') {
+		TikiLib::lib('smarty')->assign('only_shortcuts', 'y');
+	}
+	if ($module_params['display'] == 'history') {
+		TikiLib::lib('smarty')->assign('only_prefs_history', 'y');
+		TikiLib::lib('smarty')->assign('recent_prefs', TikiLib::lib('prefs')->getRecent());
+	}
+	if ($module_params['display'] == 'both') {
+		TikiLib::lib('smarty')->assign('recent_prefs', TikiLib::lib('prefs')->getRecent());
+	}
 }
