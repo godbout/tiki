81a05cbb5f7d24879a752df2b1084a2e709d552e
pom2ter <pom2ter@users.sourceforge.net>
2019-09-17 01:11:54 +0000

    [bp/r70989][FIX] cypht: issues with buttons

--- cypht/site.js
+++ cypht/site.js
@@ -4714,11 +4714,11 @@ $(function() {
         if (! $('body').hasClass('mobile')) $('body').addClass('mobile');
     }
 
-    $('.inline-cypht input[type=button]').each(function () {
-        $(this).addClass('btn').addClass('btn-secondary');
+    $('.inline-cypht input[type=button]').each(function() {
+    	$(this).addClass('btn').addClass('btn-secondary');
     })
-    $('.inline-cypht input[type=submit]').each(function () {
-        $(this).addClass('btn').addClass('btn-primary');
+    $('.inline-cypht input[type=submit]').each(function() {
+    	$(this).addClass('btn').addClass('btn-primary');
     })
 
     if (! $('body').hasClass('tiki-cypth')) $('body').addClass('tiki-cypht');
