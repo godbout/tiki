da8744f1e1afcd095e98bef2fbc2221059bd5f34
Victor Emanouilov <kroky6@gmail.com>
2019-09-24 04:31:07 +0000

    [FIX] forgot to update db/tiki.sql with caldav schema changes (thanks Jonny)

--- db/tiki.sql
+++ db/tiki.sql
@@ -410,6 +410,7 @@ CREATE TABLE `tiki_calendar_items` (
   `created` int(14) NOT NULL default '0',
   `lastmodif` int(14) NOT NULL default '0',
   `allday` tinyint(1) NOT NULL default '0',
+  `uid` varchar(200),
   PRIMARY KEY (`calitemId`),
   KEY `calendarId` (`calendarId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 ;
@@ -451,6 +452,38 @@ CREATE TABLE `tiki_calendars` (
   PRIMARY KEY (`calendarId`)
 ) ENGINE=MyISAM ;
 
+DROP TABLE IF EXISTS `tiki_calendar_changes`;
+CREATE TABLE tiki_calendar_changes (
+    changeId INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    calitemId INT(11) UNSIGNED NOT NULL,
+    synctoken INT(11) UNSIGNED NOT NULL,
+    calendarId INT(11) UNSIGNED NOT NULL,
+    operation TINYINT(1) NOT NULL,
+    INDEX (calendarId, synctoken),
+    INDEX (calitemId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `tiki_calendar_instances`;
+CREATE TABLE tiki_calendar_instances (
+    calendarInstanceId INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    calendarId INT UNSIGNED NOT NULL,
+    user VARCHAR(200),
+    access TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1 = owner, 2 = read, 3 = readwrite',
+    name VARCHAR(100),
+    uri VARBINARY(200),
+    description TEXT,
+    `order` INT(11) UNSIGNED NOT NULL DEFAULT '0',
+    color VARBINARY(10),
+    timezone TEXT,
+    transparent TINYINT(1) NOT NULL DEFAULT '0',
+    share_href VARBINARY(100),
+    share_name VARCHAR(100),
+    share_invite_status TINYINT(1) NOT NULL DEFAULT '2' COMMENT '1 = noresponse, 2 = accepted, 3 = declined, 4 = invalid',
+    UNIQUE(user, uri),
+    UNIQUE(calendarid, user),
+    UNIQUE(calendarid, share_href)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 DROP TABLE IF EXISTS `tiki_calendar_options`;
 CREATE TABLE `tiki_calendar_options` (
     `calendarId` int(14) NOT NULL default 0,
@@ -458,6 +491,34 @@ CREATE TABLE `tiki_calendar_options` (
     `value` varchar(255),
     PRIMARY KEY (`calendarId`,`optionName`)
 ) ENGINE=MyISAM ;
+
+DROP TABLE IF EXISTS `tiki_calendar_scheduling_objects`;
+CREATE TABLE tiki_calendar_scheduling_objects (
+    schedulingObjectId INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user VARCHAR(200),
+    calendardata MEDIUMBLOB,
+    uri VARBINARY(200),
+    lastmodif INT(11) UNSIGNED,
+    etag VARBINARY(32),
+    size INT(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `tiki_calendar_subscriptions`;
+CREATE TABLE tiki_calendar_subscriptions (
+    subscriptionId INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    calendarId INT(11) UNSIGNED NOT NULL,
+    user VARCHAR(200) NOT NULL,
+    source TEXT,
+    name VARCHAR(100),
+    refresh_rate VARCHAR(10),
+    `order` INT(11) UNSIGNED NOT NULL DEFAULT '0',
+    color VARBINARY(10),
+    strip_todos TINYINT(1) NULL,
+    strip_alarms TINYINT(1) NULL,
+    strip_attachments TINYINT(1) NULL,
+    lastmodif INT(11) UNSIGNED,
+    UNIQUE(user, calendarId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `tiki_categories`;
 CREATE TABLE `tiki_categories` (
