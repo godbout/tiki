godbout/tiki

commit f4076985ca351e5c117c88846f28e4f8ec18143e

[FIX] Fixed and iproved handling of including Javascript, CDNs user defined CDNs and minification. This is a major rewrite and impacts any place that uses JS on file level. Added a lot of comments to make inner workings clear and updated all areas using JS Files.
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@54754 b456876b-0849-0410-b77d-98878d47e9d5

lib/setup/javascript.php
@@ -61,31 +61,35 @@ if ($prefs['javascript_enabled'] == 'y') {	// we have JavaScript
 	if (file_exists('lang/' . $prefs['language'] . '/language.js')) {
 		// after the usual lib includes (up to 10) but before custom.js (50)
 		$headerlib
-			->add_jsfile('lang/' . $prefs['language'] . '/language.js', 25)
+			->add_jsfile('lang/' . $prefs['language'] . '/language.js')
 			->add_js("$.lang = '" . $prefs['language'] . "';");
 	}
 
+	
+	/** Use custom.js in lang dir if there **/
+	$language = $prefs['language'];
+	if (is_file("lang/$language/custom.js")) {
+		TikiLib::lib('header')->add_jsfile("lang/$language/custom.js");	// before styles custom.js
+	}
+	
+	if (!empty($tikidomain) && is_file("lang/$language/$tikidomain/custom.js")) {		// Note: lang tikidomain dirs not created automatically
+		TikiLib::lib('header')->add_jsfile("lang/$language/$tikidomain/custom.js");
+	}
+	
+	
 	/** Use custom.js in themes or options dir if there **/
 	$themelib = TikiLib::lib('theme');
 	$custom_js = $themelib->get_theme_path($prefs['theme'], $prefs['theme_option'], 'custom.js');
 	if (!empty($custom_js)) {
-		$headerlib->add_jsfile($custom_js, 50);
+		$headerlib->add_jsfile($custom_js);
 	} else {															// there's no custom.js in the current theme or option
 		$custom_js = $themelib->get_theme_path('', '', 'custom.js');		// so use one in the root of /themes if there
 		if (!empty($custom_js)) {
-			$headerlib->add_jsfile($custom_js, 50);
+			$headerlib->add_jsfile($custom_js);
 		}
 	}
 
-	/** Use custom.js in lang dir if there **/
-	$language = $prefs['language'];
-	if (is_file("lang/$language/custom.js")) {
-		TikiLib::lib('header')->add_jsfile("lang/$language/custom.js", 40);	// before styles custom.js
-	}
 
-	if (!empty($tikidomain) && is_file("lang/$language/$tikidomain/custom.js")) {		// Note: lang tikidomain dirs not created automatically
-		TikiLib::lib('header')->add_jsfile("lang/$language/$tikidomain/custom.js", 40);
-	}
 
 	// setup timezone array
 	$tz = TikiDate::getTimezoneAbbreviations();
@@ -220,7 +224,7 @@ var syntaxHighlighter = {
 				$fixondom = "DD_belatedPNG.fixPng($fixondom); // list of HTMLDomElements to fix separated by commas (default is none)";
 			}
 			$scriptpath = 'lib/iepngfix/DD_belatedPNG-min.js';
-			$headerlib->add_jsfile($scriptpath, 200);
+			$headerlib->add_jsfile($scriptpath, true);
 			$headerlib->add_js(
 <<<JS
 DD_belatedPNG.fix('$fixoncss'); // list of CSS selectors to fix separated by commas (default is set to fix sitelogo)

lib/ckeditor_tiki/wysiwyglib.php
@@ -48,7 +48,7 @@ class WYSIWYGLib
 		$headerlib = TikiLib::lib('header');
 
 		$headerlib->add_js_config('window.CKEDITOR_BASEPATH = "'. $tikiroot . 'vendor/ckeditor/ckeditor/";')
-			->add_jsfile('vendor/ckeditor/ckeditor/ckeditor.js', 0, true)
+			->add_jsfile('vendor/ckeditor/ckeditor/ckeditor.js', true)
 			->add_js('window.CKEDITOR.config._TikiRoot = "'.$tikiroot.'";', 1);
 
 		// Inline editing config
@@ -121,7 +121,7 @@ window.CKEDITOR.config.toolbar = ' .$cktools.';
 			$headerlib->add_js_config('window.CKEDITOR_BASEPATH = "'. $tikiroot . 'vendor/ckeditor/ckeditor/";')
 				//// for js debugging - copy _source from ckeditor distribution to libs/ckeditor to use
 				//// note, this breaks ajax page load via wikitopline edit icon
-				->add_jsfile('vendor/ckeditor/ckeditor/ckeditor.js', 0, true)
+				->add_jsfile('vendor/ckeditor/ckeditor/ckeditor.js', true)
 				->add_js('window.CKEDITOR.config._TikiRoot = "'.$tikiroot.'";', 1);
 
 			$headerlib->add_js(

lib/core/Tiki/PageCache.php
@@ -111,10 +111,11 @@ class Tiki_PageCache
 
 				if ( $cachedOutput && $cachedOutput['output'] ) {
 					$headerlib = TikiLib::lib('header');
-					if (is_array($cachedOutput['jsiles'])) {
-						foreach ($cachedOutput['jsiles'] as $rank => $files) {
+					if (is_array($cachedOutput['jsfiles'])) {
+						foreach ($cachedOutput['jsfiles'] as $rank => $files) {
 							foreach ($files as $file) {
-								$headerlib->add_jsfile($file, $rank);
+								$skip_minify = isset($cachedOutput['skip_minify']) ? true : false;
+								$headerlib->add_jsfile_by_rank($file, $rank, $skip_minify);
 							}
 						}
 					}
@@ -174,7 +175,8 @@ class Tiki_PageCache
 
 			if ($this->headerLibCopy) {
 				$headerlib = TikiLib::lib('header');
-				$cachedOutput['jsiles']     = array_diff($headerlib->jsfiles, $this->headerLibCopy->jsfiles);
+				$cachedOutput['jsfiles']    = array_diff($headerlib->jsfiles, $this->headerLibCopy->jsfiles);
+				$cachedOutput['skip_minify']= array_diff($headerlib->skip_minify, $this->headerLibCopy->skip_minify);
 				$cachedOutput['jq_onready'] = array_diff($headerlib->jq_onready, $this->headerLibCopy->jq_onready);
 				$cachedOutput['js']         = array_diff($headerlib->js, $this->headerLibCopy->js);
 				$cachedOutput['css']        = array_diff($headerlib->css, $this->headerLibCopy->css);

lib/core/WikiPlugin/Negotiator/Wiki.php
@@ -596,11 +596,6 @@ class WikiPlugin_Negotiator_Wiki
 				}
 			}
 
-			$headerlib->add_jsfile('tiki-jsplugin.php?language='.$this->prefs['language'], 'dynamic');
-			if ($this->prefs['wikiplugin_module'] === 'y' && $this->prefs['wikiplugininline_module'] === 'n') {
-				$headerlib->add_jsfile('tiki-jsmodule.php?language='.$this->prefs['language'], 'dynamic');
-			}
-
 			$headerlib->add_jq_onready(
 				'$("#' . $id . '")
 					.click( function(event) {'

lib/setup/mobile.php
@@ -59,8 +59,8 @@ if ( !isset($_REQUEST['mobile_mode']) || $_REQUEST['mobile_mode'] === 'y' ) {
 
 				if ($prefs['feature_shadowbox'] === 'y') {
 					$headerlib
-						->add_jsfile('vendor/jquery/photoswipe/lib/klass.min.js', 'external')
-						->add_jsfile('vendor/jquery/photoswipe/code.photoswipe.jquery-3.0.5.min.js', 'external')
+						->add_jsfile_external('vendor/jquery/photoswipe/lib/klass.min.js', true)
+						->add_jsfile_external('vendor/jquery/photoswipe/code.photoswipe.jquery-3.0.5.min.js', true)
 						->add_cssfile('vendor/jquery/photoswipe/photoswipe.css');
 				}
 

lib/sheet/sheetlib.php
@@ -395,9 +395,9 @@ class SheetLib extends TikiLib
                 ->add_jsfile( 'vendor/jquery/jquery-sheet/plugins/globalize.js' )
                 ->add_jsfile( 'vendor/jquery/jquery-sheet/plugins/globalize.cultures.js' )
 
-                ->add_jsfile( 'vendor/jquery/jquery-sheet/plugins/jquery.nearest.min.js', 'external' )
-                ->add_jsfile( 'vendor/jquery/jquery-sheet/plugins/raphael-min.js', 'external' )
-                ->add_jsfile( 'vendor/jquery/jquery-sheet/plugins/g.raphael-min.js', 'external' );
+                ->add_jsfile_external( 'vendor/jquery/jquery-sheet/plugins/jquery.nearest.min.js', true )
+                ->add_jsfile_external( 'vendor/jquery/jquery-sheet/plugins/raphael-min.js', true )
+                ->add_jsfile_external( 'vendor/jquery/jquery-sheet/plugins/g.raphael-min.js', true );
 
 			$this->setup_jQuery_sheet_files = true;
 		}

lib/tikilib.php
@@ -5467,9 +5467,11 @@ class TikiLib extends TikiDb_Bridge
 
 
 	/**
-	 * @return string
+	 * Get URL Scheme (http / https)
+	 * Considers the use of a reverse proxy / ssl offloader. I.e If request is https -> ssl offloader -> http tiki, then it will correctly return https 
+	 * @return string http | https
 	 */
-	function httpScheme()
+	static function httpScheme()
 	{
 		global $url_scheme;
 		return $url_scheme;

commit 26eaa066b3c3d245ed84122ccfc63866f139f8f5

[FIX] lang: Make custom.js lang files work again (following r63968 but caused by r54754, so not totally my fault ;)

lib/setup/javascript.php
@@ -100,11 +100,11 @@ if ($prefs['javascript_enabled'] == 'y') {	// we have JavaScript
 	/** Use custom.js in lang dir if there **/
 	$language = $prefs['language'];
 	if (is_file("lang/$language/custom.js")) {
-		TikiLib::lib('header')->add_jsfile("lang/$language/custom.js");	// before styles custom.js
+		TikiLib::lib('header')->add_jsfile_late("lang/$language/custom.js");	// before styles custom.js
 	}
 
 	if (! empty($tikidomain) && is_file("lang/$language/$tikidomain/custom.js")) {		// Note: lang tikidomain dirs not created automatically
-		TikiLib::lib('header')->add_jsfile("lang/$language/$tikidomain/custom.js");
+		TikiLib::lib('header')->add_jsfile_late("lang/$language/$tikidomain/custom.js");
 	}
 
 