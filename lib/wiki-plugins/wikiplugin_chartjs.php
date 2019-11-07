ceac7f9337a5934f97ab29873b84862bba054d38
Jorge Sá Pereira <me@jorgesapereira.com>
2019-11-07 15:28:27 +0000

    [NEW] Enable PDF export of ChartJS images
    - CasperJS is required. Use package jerome-breton/casperjs-installer

--- lib/wiki-plugins/wikiplugin_chartjs.php
+++ lib/wiki-plugins/wikiplugin_chartjs.php
@@ -5,6 +5,9 @@
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
 
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessTimedOutException;
+
 function wikiplugin_chartjs_info()
 {
 	return [
@@ -87,6 +90,8 @@ function wikiplugin_chartjs_info()
 
 function wikiplugin_chartjs($data, $params)
 {
+	global $base_url;
+
 	static $instance = 0;
 	$instance++;
 
@@ -135,20 +140,98 @@ function wikiplugin_chartjs($data, $params)
 		}
 	}
 
-	$min = $params['debug'] ? '': 'min.';
+	$to_PDF = ! empty($_REQUEST['display']) && $_REQUEST['display'] == 'pdf';
+	$min = $params['debug'] ? '' : 'min.';
 
-	TikiLib::lib('header')->add_jsfile("vendor_bundled/vendor/npm-asset/chart.js/dist/Chart.bundle.{$min}js")
-		->add_jq_onready('
-setTimeout(function () {
+	// Disable animation
+	if ($to_PDF) {
+		$options = 'animation: { duration: 0 }';
+	}
+
+	$script = '
 	var chartjs_' . $params['id'] . ' = new Chart("' . $params['id'] . '", {
 		type: "' . $params['type'] . '",
 		data: ' . json_encode($data) . ',
-		options: ' . json_encode($options) . '
-	})
-},
-500);');
+		options: { ' . $options . ' }
+	});
+';
 
-	return '<div class="tiki-chartjs" style="width:' . $params['width'] . 'px;height:' . $params['height'] . 'px">' .
-				'<canvas id="' . $params['id'] . '" width="' . $params['width'] . '" height="' . $params['height'] . '">' .
-			'</canvas></div>';
+	$canvas = '<canvas id="' . $params['id'] . '" width="' . $params['width'] . '" height="' . $params['height'] . '"></canvas>';
+
+	if (! $to_PDF) {
+		TikiLib::lib('header')->add_jsfile("vendor_bundled/vendor/npm-asset/chart.js/dist/Chart.bundle.{$min}js")
+			->add_jq_onready('setTimeout(function () {' . $script . '}, 500);');
+
+		return '<div class="tiki-chartjs">' . $canvas . '</div>';
+	}
+
+	// PDF export related logic
+	$html_content = <<<HTML
+<script src="{$base_url}vendor_bundled/vendor/npm-asset/chart.js/dist/Chart.bundle.{$min}js"></script>
+<div>
+	$canvas
+</div>
+
+<script>
+	$script
+</script>
+HTML;
+
+	$scriptHash = md5($script);
+	$cacheKey = 'chart_';
+	$cacheLib = TikiLib::lib('cache');
+
+	if (! $cacheLib->isCached($scriptHash, $cacheKey)) {
+		$htmlFile = writeTempFile($html_content, '', true, 'wikiplugin_chart_', '.html');
+		$casperBin = implode(DIRECTORY_SEPARATOR, [TIKI_PATH, 'bin', 'casperjs']);
+		if (! file_exists($casperBin)) {
+			return tr('Tiki needs the jerome-breton/casperjs-installer to convert charts to PNG. If you do not have permission to install this package, ask the site administrator.');
+		}
+
+		$casperjsScript = <<<JS
+var casper = require('casper').create();
+
+casper.start('{$htmlFile}', function() {
+	this.echo(this.captureBase64('png', 'div'));
+});
+
+casper.run();
+JS;
+
+		$casperFile = writeTempFile($casperjsScript, '', true, 'wikiplugin_chart_', '.js');
+
+		$command = $casperBin . ' ' . $casperFile;
+		$process = new Process($command);
+		if (! empty($params['timeout'])) {
+			$process->setTimeout($params['timeout']);
+			$process->setIdleTimeout($params['timeout']);
+		}
+		try {
+			$process->run();
+		} catch (ProcessTimedOutException $e) {
+			$logsLib = TikiLib::lib('logs');
+			$logsLib->add_log('Casperjs', $e->getMessage());
+
+			\Feedback::error(tr('Failed to generate chart image using Casperjs. Please check Tiki Action Log for more information.'));
+		} finally {
+			unlink($htmlFile);
+			unlink($casperFile);
+		}
+
+		if ($process->isSuccessful()) {
+			$base64 = $process->getOutput();
+		} else {
+			return false;
+		}
+
+		$cacheLib->cacheItem($scriptHash, $base64, $cacheKey);
+	} else {
+		$base64 = $cacheLib->getCached($scriptHash, $cacheKey);
+	}
+
+	$canvas = <<<HTML
+<img src="data:image/jpeg;charset=utf-8;base64, {$base64}"/>
+HTML;
+
+	return '<div class="tiki-chartjs">' . $canvas . '</div>';
 }
