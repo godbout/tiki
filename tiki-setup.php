dff657d6d698d60e7880d6cc5577ab4aa9d6602c
lyabs243 <loicyabili@outlook.com>
2019-12-04 15:43:09 +0200

    [FIX] Menus: Fix Smartmenus css issues
    (refs https://gitlab.com/tikiwiki/tiki/merge_requests/216)

--- tiki-setup.php
+++ tiki-setup.php
@@ -511,6 +511,7 @@ if ($prefs['jquery_smartmenus_enable'] == 'y') {
 	$headerlib->add_jsfile('vendor_bundled/vendor/drmonty/smartmenus/js/jquery.smartmenus.js');
 	$headerlib->add_jsfile('vendor_bundled/vendor/drmonty/smartmenus/js/jquery.smartmenus.bootstrap-4.js');
 	$headerlib->add_cssfile('vendor_bundled/vendor/drmonty/smartmenus/css/sm-core-css.css');
+	$headerlib->add_cssfile('vendor_bundled/vendor/drmonty/smartmenus/css/jquery.smartmenus.bootstrap-4.css');
 	if (! empty($prefs['jquery_smartmenus_mode'])) {
 		$headerlib->add_cssfile(
 			'vendor_bundled/vendor/drmonty/smartmenus/css/sm-' . $prefs['jquery_smartmenus_mode'] . '.css'
