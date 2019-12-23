03b5c17edb7c56c2636f1f03d8f965115e6f6b65
lindonb <lindonb@users.sourceforge.net>
2019-12-23 02:12:14 +0000

    [MRG] Automatic merge, branches/20.x 75194 to 75199

--- tiki-setup.php
+++ tiki-setup.php
@@ -336,7 +336,7 @@ $smarty->assign('symbols', TikiLib::symbols());
 
 // Used by TikiAccessLib::redirect()
 if (isset($_GET['msg'])) {
-	Feedback::add(['mes' => $_GET['msg'], 'type' => $_GET['msgtype']]);
+	Feedback::add(['mes' => htmlspecialchars($_GET['msg']), 'type' => htmlspecialchars($_GET['msgtype'])]);
 } elseif (isset($_SESSION['msg'])) {
 	Feedback::add(['mes' => $_SESSION['msg'], 'type' => $_SESSION['msgtype']]);
 	unset($_SESSION['msg']);
