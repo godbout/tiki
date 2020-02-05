966baeae90398063a87896b5e7497e24cc0afcba
Jonny Bradley <jmail@nospaces.net>
2020-02-05 15:12:33 +0000

    [FIX] setup: Check if buffer exists before cleaning it, eliminates new error from console.php

--- lib/setup/output_compression.php
+++ lib/setup/output_compression.php
@@ -23,7 +23,9 @@ if (! empty($force_no_compression) && $force_no_compression) {
 	// if not, check if tiki compression is enabled
 	} elseif ($prefs['feature_obzip'] == 'y') {
 		// tiki compression is enabled, then let activate the handler
-		@ob_clean();					// but make sure it's empty first
+		if (ob_get_length()) {
+			ob_clean();                    // but make sure it's empty first
+		}
 		ob_start('ob_gzhandler');
 		$smarty->assign('gzip_handler', 'tiki');
 		$smarty->assign('gzip', 'Enabled');
