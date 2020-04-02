6481c152d467ce08c6d263907999d8ddae4ee11c
drsassafras <drsassafras@gmail.com>
2020-04-02 10:34:24 -0400

    [ENH] Add unit tests for all of our tiki filters

--- lib/test/core/TikiFilter/MapTest.php
+++ lib/test/core/TikiFilter/MapTest.php
@@ -14,17 +14,44 @@ class TikiFilter_MapTest extends TikiTestCase
 {
 	function testDirect()
 	{
+		$this->assertTrue(TikiFilter::get('int') instanceof Zend\Filter\ToInt);
+		$this->assertTrue(TikiFilter::get('bool') instanceof Zend\Filter\Boolean);
+		$this->assertTrue(TikiFilter::get('isodate') instanceof TikiFilter_IsoDate);
+		$this->assertTrue(TikiFilter::get('isodatetime') instanceof TikiFilter_IsoDate);
+		$this->assertTrue(TikiFilter::get('iso8601') instanceof TikiFilter_IsoDate);
+		$this->assertTrue(TikiFilter::get('attribute_type') instanceof TikiFilter_AttributeType);
+		$this->assertTrue(TikiFilter::get('lang') instanceof TikiFilter_Lang);
+		$this->assertTrue(TikiFilter::get('relativeurl') instanceof TikiFilter_RelativeURL);
 		$this->assertTrue(TikiFilter::get('digits') instanceof Zend\Filter\Digits);
+		$this->assertTrue(TikiFilter::get('digitscolons') instanceof Zend\Filter\PregReplace);
+		$this->assertTrue(TikiFilter::get('digitscommas') instanceof Zend\Filter\PregReplace);
+		$this->assertTrue(TikiFilter::get('digitspipes') instanceof Zend\Filter\PregReplace);
 		$this->assertTrue(TikiFilter::get('alpha') instanceof TikiFilter_Alpha);
+		$this->assertTrue(TikiFilter::get('alphaspace') instanceof TikiFilter_Alpha);
+		$this->assertTrue(TikiFilter::get('word') instanceof Zend\Filter\PregReplace);
+		$this->assertTrue(TikiFilter::get('wordspace') instanceof Zend\Filter\PregReplace);
 		$this->assertTrue(TikiFilter::get('alnum') instanceof TikiFilter_Alnum);
-		$this->assertTrue(TikiFilter::get('striptags') instanceof Zend\Filter\StripTags);
-		$this->assertTrue(TikiFilter::get('pagename') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('alnumdash') instanceof Zend\Filter\PregReplace);
+		$this->assertTrue(TikiFilter::get('alnumspace') instanceof TikiFilter_Alnum);
 		$this->assertTrue(TikiFilter::get('username') instanceof Zend\Filter\StripTags);
 		$this->assertTrue(TikiFilter::get('groupname') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('pagename') instanceof Zend\Filter\StripTags);
 		$this->assertTrue(TikiFilter::get('topicname') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('themename') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('email') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('url') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('text') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('date') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('time') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('datetime') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('striptags') instanceof Zend\Filter\StripTags);
+		$this->assertTrue(TikiFilter::get('purifier') instanceof TikiFilter_HtmlPurifier);
+		$this->assertTrue(TikiFilter::get('html') instanceof TikiFilter_HtmlPurifier);
 		$this->assertTrue(TikiFilter::get('xss') instanceof TikiFilter_PreventXss);
-		$this->assertTrue(TikiFilter::get('word') instanceof Zend\Filter\PregReplace);
 		$this->assertTrue(TikiFilter::get('wikicontent') instanceof TikiFilter_WikiContent);
+		$this->assertTrue(TikiFilter::get('rawhtml_unsafe') instanceof TikiFilter_RawUnsafe);
+		$this->assertTrue(TikiFilter::get('none') instanceof TikiFilter_None);
+
 	}
 
 	function testKnown()
