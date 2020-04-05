58c6715692e719baa2799875e8fb57541dc5876d
Jean-Marc Libs <jyhem@tiki.org>
2020-04-05 22:48:45 +0200

    [FIX] There was an extra debug line in commit 80399e77c65fa5bc5a7b5ae1d3a7e1549d115bae (oops) and syntax warnings

--- installer/schema/20140705_page_slug_tiki.php
+++ installer/schema/20140705_page_slug_tiki.php
@@ -13,12 +13,12 @@ function post_20140705_page_slug_tiki($installer)
 			// Presumably, Duplicate entry found. Too bad $e->getCode() returns 0
 			// We need to find a pageSlug name which is not already taken
 			// Shorten so we can add numbers afterwards in the form blabla…bla_42 from _0 to _999
-			$prefixPageSlug = substr(urlencode($name),0,156);
+			$prefixPageSlug = substr(urlencode($name), 0, 156);
 			// Strip urlencoding %xx which might have been truncated at the end
-			if (strrpos($prefixPageSlug,'%') && strrpos($prefixPageSlug,'%') >= 154) {
-				$prefixPageSlug = substr($prefixPageSlug,0,strrpos($prefixPageSlug,'%'));
+			if (strrpos($prefixPageSlug, '%') && strrpos($prefixPageSlug, '%') >= 154) {
+				$prefixPageSlug = substr($prefixPageSlug, 0, strrpos($prefixPageSlug, '%'));
 			}
-			for ($i=0;$i<=999;$i++) {
+			for ($i = 0; $i <= 999; $i++) {
 				$pageSlug = $prefixPageSlug . "_$i";
 				$duplicates = $pages->fetchColumn('pageName', ['pageSlug' => $pageSlug ]);
 				if (count($duplicates) == 0) {
