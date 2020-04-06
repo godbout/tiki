996588fa5ee3aa55a08960345cf8ed0155d7ef38
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2020-04-06 16:05:56 -0300

    [rb/r75874] Revert "[FIX] Currecy Field : add server-side validation"
    This reverts commit fb0296cfbaa41078b4e66f047c34b1cc8887ebab.
    (refs https://gitlab.com/tikiwiki/tiki/-/merge_requests/280)

--- lib/trackers/trackerlib.php
+++ lib/trackers/trackerlib.php
@@ -2667,12 +2667,6 @@ class TrackerLib extends TikiLib
 		$erroneous_values = [];
 		if (isset($ins_fields)&&isset($ins_fields['data'])) {
 			foreach ($ins_fields['data'] as $f) {
-				if ($f['type'] == 'b' && ! empty($f['value'])){
-					$ins_id = 'ins_' . $f['fieldId'];
-					if (empty($_REQUEST[$ins_id . '_currency']) ) {
-						$erroneous_values[] = $f;
-					}
-				}
 				if ($f['type'] == 'f' && $f['isMandatory'] != 'y' && empty($f['value'])) {
 					$ins_id = 'ins_' . $f['fieldId'];
 					if (! empty($_REQUEST[$ins_id . 'Month']) || ! empty($_REQUEST[$ins_id . 'Day']) || ! empty($_REQUEST[$ins_id . 'Year']) ||
