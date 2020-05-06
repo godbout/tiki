74ebb1ad06a64f6a2fef0c4837a0fb1789d469af
Andon Andonov <andon.andonov85@gmail.com>
2020-05-06 12:31:49 +0300

    Fixed chronometer total milliseconds sync; added start/stop buttons and styling

--- lib/vue/duration/DurationPickerChronometer.vue
+++ lib/vue/duration/DurationPickerChronometer.vue
@@ -1,19 +1,27 @@
 <template>
 	<div class="dp-chronometer__container">
         <div class="dp-chronometer__group">
-            <span class="dp-chronometer-btn unselectable" v-on:click="startTimer">start at:</span>
+            <span class="dp-chronometer-btn unselectable">
+                <i class="fas fa-play" v-show="show" v-on:click="startTimer"></i>
+            </span>
+            <span class="dp-chronometer-btn unselectable">
+                <i class="fas fa-pause" v-show="!show" v-on:click="stopTimer"></i>
+            </span>
+            <span class="dp-chronometer-btn unselectable" v-on:click="resetTimer">
+                <i class="fas fa-undo-alt"></i>
+            </span>
+        </div>
+        <div class="dp-chronometer__group">
+            <span class="unselectable">start at:</span>
             <span class="dp-chronometer__info">{{ formatStartTime }}</span>
         </div>
         <div class="dp-chronometer__group">
-            <span class="dp-chronometer-btn unselectable" v-on:click="stopTimer">stop at:</span>
+            <span class="unselectable">stop at:</span>
             <span class="dp-chronometer__info">{{ formatStopTime }}</span>
         </div>
-        <!-- <div class="dp-chronometer__group">
-            <span>total time:</span>
-            <span class="dp-chronometer__info">{{ formatedTotalTime }}</span>
-        </div> -->
         <div class="dp-chronometer__group">
-            <span class="dp-chronometer-btn unselectable" v-on:click="resetTimer">reset</span>
+            <span>total time:</span>
+            <span class="dp-chronometer__info">{{ calcSpentTime }}</span>
         </div>
 	</div>
 </template>
@@ -31,16 +39,16 @@ export default {
             initialDurationMilliseconds: 0,
             initialTime: 0,
             totalTime: 0,
-            formatedTotalTime: '--'
+            show: true
         };
     },
     beforeDestroy: function () {
         clearInterval(this.interval);
     },
     computed: {
-        // calcSpentTime: function () {
-        //     return moment.duration(this.initialTime + this.currentTime).format('h [hrs], m [min], s [sec]');
-        // },
+        calcSpentTime: function () {
+            return moment.duration(this.totalTime).format('h [hrs], m [min], s [sec], S [ms]');
+        },
         formatStartTime: function () {
             if (this.startTime !== '--') {
                 return moment(this.startTime).format('YYYY-MM-DD HH:mm:ss');
@@ -59,15 +67,18 @@ export default {
     methods: {
         startTimer: function () {
             if (!this.interval) {
+                this.show = false;
                 this.initialDurationMilliseconds = moment.duration(this.store.state.duration.amounts).asMilliseconds();
-                // this.initialTime = this.totalTime;
+
+                this.initialTime = this.totalTime;
+
                 this.startTime = moment();
 
                 this.interval = setInterval(() => {
                     let millisecondsDiff = moment().diff(this.startTime);
                     this.store.setDuration(this.initialDurationMilliseconds + millisecondsDiff);
 
-                    // this.totalTime = this.initialTime + millisecondsDiff;
+                    this.totalTime = this.initialTime + millisecondsDiff;
                 }, 100);
 
                 this.stopTime = '--';
@@ -75,11 +86,9 @@ export default {
         },
         stopTimer: function () {
             clearInterval(this.interval);
+            this.show = true;
             this.interval = false;
-            
             this.stopTime = moment();
-
-            // this.formatedTotalTime = moment.duration(this.totalTime).format('h [hrs], m [min], s [sec]');
         },
         resetTimer: function () {
             clearInterval(this.interval);
@@ -88,9 +97,8 @@ export default {
             this.store.setDuration(this.initialAmounts);
             this.startTime = '--';
             this.stopTime = '--';
-            // this.initialTime = 0;
-            // this.totalTime = 0;
-            // this.formatedTotalTime = '--';
+            this.initialTime = 0;
+            this.totalTime = 0;
         }
     }
 };
