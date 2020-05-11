ff8f8255f28ab510af62eb264d08f44a63a7306d
lindonb <lindon@tiki.org>
2020-05-10 20:45:02 -0400

    [FIX] banning: Improve error method and declare global $prefs variable

--- tiki-section_options.php
+++ tiki-section_options.php
@@ -7,15 +7,15 @@
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
-
-if ($prefs['feature_theme_control'] == 'y') {
+global $prefs;
+if ($prefs['feature_theme_control'] == 'y' && (! isset($banningOnly) || ! $banningOnly)) {
 	include('tiki-tc.php');
 }
 if ($prefs['feature_banning'] == 'y') {
 	global $user;
 	if ($msg = TikiLib::lib('tiki')->check_rules($user, $section)) {
 		if (isset($ajaxRequest) && $ajaxRequest) {
-			Services_Utilities::modalException(tra('No users were selected. Please select one or more users.'));
+			Services_Utilities::modalException(tr('You are banned from %0', $section));
 		} else {
 			Feedback::errorPage($msg);
 		}
