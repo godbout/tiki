f1d0a9ffb097fa05b650b9fd763a85b101c665b5
Jonny Bradley <jmail@nospaces.net>
2020-05-13 17:52:54 +0100

    [FIX] trackers: Show a decent error message if the argument for a rule is not provided but is required

--- lib/core/Tracker/Rule/Rules.php
+++ lib/core/Tracker/Rule/Rules.php
@@ -117,6 +117,8 @@ class Rules
 		$syntax = $operatorObject->getSyntax();
 		if ($predicate->argument !== null) {
 			$syntax = str_replace('%argument%', $predicate->argument, $syntax);
+		} else if (strpos($syntax, '%argument%') !== false) {
+			$syntax = str_replace('%argument%', tr('No argument for "%0" rule', $predicate->operator_id), $syntax);
 		}
 
 		return $syntax;
